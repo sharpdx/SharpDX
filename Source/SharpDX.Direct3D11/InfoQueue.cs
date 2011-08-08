d43a6740412c0c09ac974d8cf754d429667628fd
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2011-08-08 10:56:18 +0900

    [Direct3D10/11] Add support for InfoQueue

---
+++ Source/SharpDX.Direct3D11/InfoQueue.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) 2010-2011 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+
+namespace SharpDX.Direct3D11
+{
+    public partial class InfoQueue
+    {
+        /// <summary>	
+        /// No documentation.	
+        /// </summary>	
+        /// <param name="messageIndex">No documentation.</param>	
+        /// <returns>No documentation.</returns>	
+        /// <include file='.\..\Documentation\CodeComments.xml' path="/comments/comment[@id='ID3D11InfoQueue::GetMessageW']/*"/>	
+        /// <unmanaged>HRESULT ID3D11InfoQueue::GetMessageW([In] unsigned longlong MessageIndex,[Out, Buffer, Optional] D3D11_MESSAGE* pMessage,[InOut] SIZE_T* pMessageByteLength)</unmanaged>	
+        public Message GetMessage(long messageIndex)
+        {
+            Size messageSize = 0;
+            GetMessage(messageIndex, IntPtr.Zero, ref messageSize);
+
+            var message = new Message {DescriptionByteLength = messageSize};
+            var messageNative = new Message.__Native();
+            message.__MarshalTo(ref messageNative);
+
+            unsafe
+            {
+                GetMessage(messageIndex, new IntPtr(&messageNative), ref messageSize);
+            }
+
+            message.__MarshalFrom(ref messageNative);
+            message.__MarshalFree(ref messageNative);
+            return message;
+        }
+
+        /// <summary>	
+        /// No documentation.	
+        /// </summary>	
+        /// <returns>No documentation.</returns>	
+        /// <include file='.\..\Documentation\CodeComments.xml' path="/comments/comment[@id='ID3D11InfoQueue::GetStorageFilter']/*"/>	
+        /// <unmanaged>HRESULT ID3D11InfoQueue::GetStorageFilter([Out, Buffer, Optional] D3D11_INFO_QUEUE_FILTER* pFilter,[InOut] SIZE_T* pFilterByteLength)</unmanaged>	
+        public SharpDX.Direct3D11.InfoQueueFilter GetStorageFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>	
+        /// No documentation.	
+        /// </summary>	
+        /// <returns>No documentation.</returns>	
+        /// <include file='.\..\Documentation\CodeComments.xml' path="/comments/comment[@id='ID3D11InfoQueue::GetRetrievalFilter']/*"/>	
+        /// <unmanaged>HRESULT ID3D11InfoQueue::GetRetrievalFilter([Out, Buffer, Optional] D3D11_INFO_QUEUE_FILTER* pFilter,[InOut] SIZE_T* pFilterByteLength)</unmanaged>	
+        public SharpDX.Direct3D11.InfoQueueFilter GetRetrievalFilter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Source/SharpDX.Direct3D11/Message.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) 2010-2011 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace SharpDX.Direct3D11
+{
+    public partial struct Message
+    {
+        // Internal native struct used for marshalling
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        internal unsafe partial struct __Native
+        {
+            public SharpDX.Direct3D11.MessageCategory Category;
+            public SharpDX.Direct3D11.MessageSeverity Severity;
+            public SharpDX.Direct3D11.MessageId Id;
+            public System.IntPtr PDescription;
+            public SharpDX.Size DescriptionByteLength;
+            // Method to free unmanaged allocation
+            internal unsafe void __MarshalFree()
+            {
+                if (this.PDescription != IntPtr.Zero)
+                    Marshal.FreeHGlobal(this.PDescription);
+            }
+        }
+
+        // Method to free unmanaged allocation
+        internal unsafe void __MarshalFree(ref __Native @ref)
+        {
+            @ref.__MarshalFree();
+        }
+
+        // Method to marshal from native to managed struct
+        internal unsafe void __MarshalFrom(ref __Native @ref)
+        {
+            this.Category = @ref.Category;
+            this.Severity = @ref.Severity;
+            this.Id = @ref.Id;
+            this.Description = (@ref.PDescription == IntPtr.Zero) ? null : new string((sbyte*)@ref.PDescription, 0, @ref.DescriptionByteLength);
+            this.DescriptionByteLength = @ref.DescriptionByteLength;
+        }
+        // Method to marshal from managed struct tot native
+        internal unsafe void __MarshalTo(ref __Native @ref)
+        {
+            @ref.Category = this.Category;
+            @ref.Severity = this.Severity;
+            @ref.Id = this.Id;
+            @ref.PDescription = Marshal.AllocHGlobal((int)this.DescriptionByteLength);
+            @ref.DescriptionByteLength = this.DescriptionByteLength;
+        }
+    }
+}
+
