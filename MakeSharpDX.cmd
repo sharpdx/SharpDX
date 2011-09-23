c72ba0b8913fe4de69395a3273691979ba844d10
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2011-09-23 17:00:59 +0900

    [Build] Build improved. Add nuget and Win8 build.

--- MakeSharpDX.cmd
+++ MakeSharpDX.cmd
@@ -1,6 +1,5 @@
+@echo off
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\vc\vcvarsall.bat" x86
 set DXROOT=C:\Program Files\Sandcastle
-set PATH=..\nant\bin;%PATH%
-nant %1 %2 %3 %4 %5 %6
-pause
+%~dp0\External\nant\bin\nant %*
