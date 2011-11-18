d57c466ba6b19e220f2183ba9f235ead0a017c26
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2011-11-19 07:53:51 +0900

    [Build] Prepare for new nuget release

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly:AssemblyVersion("2.0.0.0")]
-[assembly:AssemblyFileVersion("2.0.0.0")]
+[assembly:AssemblyVersion("2.0.1")]
+[assembly:AssemblyFileVersion("2.0.1")]
 
 #if DEBUG
 [assembly:AssemblyConfiguration("Debug")]
