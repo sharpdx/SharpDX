78bb6ff4b17eab5bb252bf4b04bb763028f0ec35
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2011-11-22 22:17:55 +0900

    [Build] Update ChangeLog, ReleaseNotes and ShareAssemblyInfo for SharpDX 2.0 beta2 (2.0.2)

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly:AssemblyVersion("2.0.1")]
-[assembly:AssemblyFileVersion("2.0.1")]
+[assembly:AssemblyVersion("2.0.2")]
+[assembly:AssemblyFileVersion("2.0.2")]
 
 #if DEBUG
 [assembly:AssemblyConfiguration("Debug")]
