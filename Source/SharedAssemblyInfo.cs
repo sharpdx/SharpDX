87bc8c30b8375de5fdc82d0ae37e94e2cff22112
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2012-02-04 08:24:25 +0900

    [Build] Add nightly-build target to nant

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly:AssemblyVersion("2.0.3")]
-[assembly:AssemblyFileVersion("2.0.3")]
+[assembly:AssemblyVersion("2.0.4")]
+[assembly:AssemblyFileVersion("2.0.4")]
 
 #if DEBUG
 [assembly:AssemblyConfiguration("Debug")]
