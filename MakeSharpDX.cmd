a2784268a74952126725b898f5983d6da32330a0
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2012-02-04 11:54:56 +0900

    [Build] Add git-sync to build

--- MakeSharpDX.cmd
+++ MakeSharpDX.cmd
@@ -1,5 +1,6 @@
 @echo off
 setlocal
+set PATH=C:\Program Files (x86)\Git\bin\;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\vc\vcvarsall.bat" x86
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\vc\vcvarsall.bat" x86
 set DXROOT=C:\Program Files\Sandcastle
