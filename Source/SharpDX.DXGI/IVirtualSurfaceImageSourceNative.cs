ae906455833b78778a6265b3eea5095290fb6bf9
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2012-03-23 22:33:49 +0900

    [DXGI] Add support for Win8 IVirtualSurfaceUpdatesCallbackNative

---
+++ Source/SharpDX.DXGI/IVirtualSurfaceImageSourceNative.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (c) 2010-2011 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+#if WIN8
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpDX.DXGI
+{
+    public partial class IVirtualSurfaceImageSourceNative 
+    {
+        private IVirtualSurfaceUpdatesCallbackNative callback;
+        private EventHandler<EventArgs> updatesNeeded;
+
+        /// <summary>
+        /// Gets the set of regions that must be updated on the shared surface.
+        /// </summary>
+        public Rectangle[] UpdateRectangles
+        {
+            get
+            {
+                int count = this.GetUpdateRectCount();
+                var regionToUpdate = new Rectangle[count];
+                this.GetUpdateRects(regionToUpdate, count);
+                return regionToUpdate;
+            }
+        }
+
+        /// <summary>
+        /// Event fired when an update is needed. Use <see cref="IVirtualSurfaceImageSourceNative.UpdateRectangles"/> to get the region to update.
+        /// </summary>
+        public event EventHandler<EventArgs> UpdatesNeeded
+        {
+            add
+            {
+                if (callback == null)
+                {
+                    callback = new VirtualSurfaceUpdatesCallbackNativeCallback(this);
+                    RegisterForUpdatesNeeded_(VirtualSurfaceUpdatesCallbackNativeShadow.ToIntPtr(callback));
+                }
+
+                updatesNeeded = (EventHandler<EventArgs>)Delegate.Combine(updatesNeeded, value);
+
+            }
+
+            remove
+            {
+
+                updatesNeeded = (EventHandler<EventArgs>)Delegate.Remove(updatesNeeded, value);
+            }
+        }
+
+        private void OnUpdatesNeeded()
+        {
+            if (updatesNeeded != null)
+                updatesNeeded(this, EventArgs.Empty);
+        }
+
+        private class VirtualSurfaceUpdatesCallbackNativeCallback : DisposeBase, IVirtualSurfaceUpdatesCallbackNative
+        {
+            IVirtualSurfaceImageSourceNative eventCallback;
+
+            public VirtualSurfaceUpdatesCallbackNativeCallback(IVirtualSurfaceImageSourceNative eventCallbackArg)
+            {
+                eventCallback = eventCallbackArg;
+            }
+
+            public void UpdatesNeeded()
+            {
+                eventCallback.OnUpdatesNeeded();
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    if (Shadow != null)
+                    {
+                        Shadow.Dispose();
+                        Shadow = null;
+                    }
+                    eventCallback = null;
+                }
+            }
+
+            public IDisposable Shadow { get; set; }
+        }
+    }
+}
+#endif
---
+++ Source/SharpDX.DXGI/VirtualSurfaceUpdatesCallbackNativeShadow.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) 2010-2011 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+#if WIN8
+using System;
+using System.Runtime.InteropServices;
+
+namespace SharpDX.DXGI
+{
+    /// <summary>
+    /// Internal VirtualSurfaceUpdatesCallbackNative Callback
+    /// </summary>
+    internal class VirtualSurfaceUpdatesCallbackNativeShadow : SharpDX.ComObjectShadow
+    {
+        private static readonly VirtualSurfaceUpdatesCallbackNativeVtbl Vtbl = new VirtualSurfaceUpdatesCallbackNativeVtbl();
+
+        /// <summary>
+        /// Get a native callback pointer from a managed callback.
+        /// </summary>
+        /// <param name="virtualSurfaceUpdatesCallbackNative">The geometry sink.</param>
+        /// <returns>A pointer to the unmanaged geomerty sink counterpart</returns>
+        public static IntPtr ToIntPtr(IVirtualSurfaceUpdatesCallbackNative virtualSurfaceUpdatesCallbackNative)
+        {
+            return ToIntPtr<VirtualSurfaceUpdatesCallbackNativeShadow, IVirtualSurfaceUpdatesCallbackNative>(virtualSurfaceUpdatesCallbackNative);
+        }
+
+        public class VirtualSurfaceUpdatesCallbackNativeVtbl : ComObjectVtbl
+        {
+            public VirtualSurfaceUpdatesCallbackNativeVtbl() : base(1)
+            {
+                AddMethod(new UpdatesNeededDelegate(UpdatesNeededImpl));
+            }
+
+            [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+            private delegate int UpdatesNeededDelegate(IntPtr thisPtr);
+            private static int UpdatesNeededImpl(IntPtr thisPtr)
+            {
+                try
+                {
+                    var shadow = ToShadow<VirtualSurfaceUpdatesCallbackNativeShadow>(thisPtr);
+                    var callback = (IVirtualSurfaceUpdatesCallbackNative)shadow.Callback;
+                    callback.UpdatesNeeded();
+                }
+                catch (SharpDXException exception)
+                {
+                    return exception.ResultCode.Code;
+                }
+                catch (Exception)
+                {
+                    return Result.Fail.Code;
+                }
+                return Result.Ok.Code;
+            }
+        }
+
+        protected override CppObjectVtbl GetVtbl
+        {
+            get { return Vtbl; }
+        }
+    }
+}
+#endif
