8a75e9781d5d99c4355827e1a083bd5c1bd12e4a
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2012-04-04 14:50:49 +0900

    [SharpDX] Move to version to 2.1.0 for next release.

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly:AssemblyVersion("2.0.4")]
-[assembly:AssemblyFileVersion("2.0.4")]
+[assembly:AssemblyVersion("2.1.0")]
+[assembly:AssemblyFileVersion("2.1.0")]
 
 #if DEBUG
 [assembly:AssemblyConfiguration("Debug")]
