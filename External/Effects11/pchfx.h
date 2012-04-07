2faa70eed315e7f2db899d6d24f813217c356404
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2012-04-07 10:41:51 +0900

    [Effects11] Add dlls for Effects11 under Direct3D11.1 x86 and x64

--- External/Effects11/pchfx.h
+++ External/Effects11/pchfx.h
@@ -10,8 +10,36 @@
 #ifndef __D3DX11_PCHFX_H__
 #define __D3DX11_PCHFX_H__
 
+#define D3DX11_DEFAULT            ((UINT) -1)
+#define D3DX11_FROM_FILE          ((UINT) -3)
+#define DXGI_FORMAT_FROM_FILE     ((DXGI_FORMAT) -3)
+
+#ifndef D3DX11INLINE
+#ifdef _MSC_VER
+  #if (_MSC_VER >= 1200)
+  #define D3DX11INLINE __forceinline
+  #else
+  #define D3DX11INLINE __inline
+  #endif
+#else
+  #ifdef __cplusplus
+  #define D3DX11INLINE inline
+  #else
+  #define D3DX11INLINE
+  #endif
+#endif
+#endif
+
+#define _FACD3D  0x876
+#define MAKE_D3DHRESULT( code )  MAKE_HRESULT( 1, _FACD3D, code )
+#define MAKE_D3DSTATUS( code )  MAKE_HRESULT( 0, _FACD3D, code )
+
+#define D3DERR_INVALIDCALL                      MAKE_D3DHRESULT(2156)
+#define D3DERR_WASSTILLDRAWING                  MAKE_D3DHRESULT(540)
+#include <float.h>
+
 #include "d3d11.h"
-#include "d3dx11.h"
+//#include "d3dx11.h"
 #undef DEFINE_GUID
 #include "INITGUID.h"
 #include "d3dx11effect.h"
