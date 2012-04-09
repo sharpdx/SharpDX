3b3039ae6509f49ff4657c9bcd781dbafd8fbf44
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2012-04-09 23:10:39 +0900

    [SharpDX] Add NativeFile.ReadAllBytes and ReadAllText methods

--- Source/SharpDX/IO/NativeFile.cs
+++ Source/SharpDX/IO/NativeFile.cs
@@ -20,6 +20,7 @@
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace SharpDX.IO
 {
@@ -28,6 +29,65 @@ namespace SharpDX.IO
     /// </summary>
     public class NativeFile 
     {
+        /// <summary>
+        /// Opens a binary file, reads the contents of the file into a byte array, and then closes the file.
+        /// </summary>
+        /// <param name="path">The file to open for reading. </param>
+        /// <returns>A byte array containing the contents of the file.</returns>
+        public static byte[] ReadAllBytes(string path)
+        {
+            byte[] buffer;
+            using (var stream = new NativeFileStream(path, NativeFileMode.Open, NativeFileAccess.Read))
+            {
+                int offset = 0;
+                long length = stream.Length;
+                if (length > 0x7fffffffL)
+                {
+                    throw new IOException("File too long");
+                }
+
+                int count = (int)length;
+                buffer = new byte[count];
+
+                while (count > 0)
+                {
+                    int num4 = stream.Read(buffer, offset, count);
+                    if (num4 == 0)
+                    {
+                        throw new EndOfStreamException();
+                    }
+                    offset += num4;
+                    count -= num4;
+                }
+            }
+            return buffer;
+        }
+
+        /// <summary>
+        /// Opens a text file, reads all lines of the file, and then closes the file.
+        /// </summary>
+        /// <param name="path">The file to open for reading. </param>
+        /// <returns>A string containing all lines of the file.</returns>
+        public static string ReadAllText(string path)
+        {
+            return ReadAllText(path, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// Opens a text file, reads all lines of the file, and then closes the file.
+        /// </summary>
+        /// <param name="path">The file to open for reading. </param>
+        /// <returns>A string containing all lines of the file.</returns>
+        public static string ReadAllText(string path, Encoding encoding)
+        {
+            using (var stream = new NativeFileStream(path, NativeFileMode.Open, NativeFileAccess.Read))
+            {
+                using (StreamReader reader = new StreamReader(stream, encoding, true, 0x400))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
 
 #if WIN8
         /// <summary>
