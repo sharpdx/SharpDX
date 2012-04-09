1cea00df9cd997837808f402e9eee0184eb1e4c5
Jose.Fajardo.III@gmail.com <Jose.Fajardo.III@gmail.com>
2012-04-09 12:46:02 +1000

    Started to create a Custom Effect demo
    Signed-off-by: Jose.Fajardo.III@gmail.com <Jose.Fajardo.III@gmail.com>

---
+++ Samples/Win8/CustomPixelShaderEffect/EffectRenderer.cs
@@ -0,0 +1,216 @@
+﻿// Copyright (c) 2010-2011 SharpDX - Jose Fajardo
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonDX;
+using SharpDX;
+using SharpDX.Direct2D1;
+using SharpDX.DirectWrite;
+using Matrix = SharpDX.Matrix;
+
+namespace CustomPixelShaderEffect
+{
+    public class EffectRenderer : Component
+    {
+        private DeviceManager _deviceManager;
+        private SharpDX.WIC.FormatConverter _formatConverter;
+
+        private Brush sceneColorBrush;
+        private SharpDX.Direct2D1.CustomEffect _effectGraph;
+
+
+        private RippleEffect _rippleEffect;
+        
+        private Windows.UI.Xaml.UIElement _root;
+        private Windows.UI.Xaml.DependencyObject _rootParent;
+
+        public EffectRenderer(Windows.UI.Xaml.UIElement rootForPointerEvents, Windows.UI.Xaml.UIElement rootOfLayout)
+        {
+            _root = rootForPointerEvents;
+            _rootParent = rootOfLayout;
+            EnableClear = false;
+            Show = true;
+
+            _root.PointerMoved += root_PointerMoved;
+        }
+
+
+
+        public bool EnableClear { get; set; }
+
+        public bool Show { get; set; }
+        public Vector2 Scale { get; set; }
+        public Vector3 PointsAt { get; set; }
+
+
+
+
+
+        public virtual void Initialize(DeviceManager deviceManager)
+        {
+            _deviceManager = deviceManager;
+            sceneColorBrush = new SolidColorBrush(deviceManager.ContextDirect2D, Colors.White);
+            
+            //GET IMAGE DATA
+            _formatConverter = DecodeImage();
+
+            //DEFAULTS MAY NEED THIS DATA
+            var size = deviceManager.ContextDirect2D.Size;
+            int pixelWidth = (int)(size.Width * Windows.Graphics.Display.DisplayProperties.LogicalDpi / 96.0);
+            int pixelHeight = (int)(size.Height * Windows.Graphics.Display.DisplayProperties.LogicalDpi / 96.0);
+
+
+            //CREATE EFFECT-GRAPH USING IMAGE DATA
+            Scale = new Vector2(0.8f, 0.8f);
+
+            PointsAt = new Vector3(pixelWidth / 2, pixelHeight / 2, 0.0f);
+
+
+            UpdateEffectGraph();
+
+        }
+
+
+        public virtual void Render(TargetBase target)
+        {
+            return;
+            if (!Show)
+                return;
+
+            if (_effectGraph == null) return;
+
+            var context2D = target.DeviceManager.ContextDirect2D;
+
+            context2D.BeginDraw();
+
+            if (EnableClear)
+                context2D.Clear(Colors.Black);
+
+            context2D.Clear(Colors.Transparent);
+
+            //context2D.DrawImage(_effectGraph.Output, InterpolationMode.Linear, CompositeMode.DestinationAtop);
+
+
+            context2D.EndDraw();
+        }
+
+
+
+        private SharpDX.WIC.FormatConverter DecodeImage()
+        {
+            var path = Windows.ApplicationModel.Package.Current.InstalledLocation.Path;
+
+            SharpDX.WIC.BitmapDecoder bitmapDecoder = new SharpDX.WIC.BitmapDecoder(
+                                                                                        _deviceManager.WICFactory,
+                                                                                        @"Assets\SydneyOperaHouse002.png",
+                                                                                        SharpDX.IO.NativeFileAccess.Read,
+                                                                                        SharpDX.WIC.DecodeOptions.CacheOnDemand
+                                                                                    );
+
+            SharpDX.WIC.BitmapFrameDecode bitmapFrameDecode = bitmapDecoder.GetFrame(0);
+
+            SharpDX.WIC.BitmapSource bitmapSource = new SharpDX.WIC.BitmapSource(bitmapFrameDecode.NativePointer);
+
+            SharpDX.WIC.FormatConverter formatConverter = new SharpDX.WIC.FormatConverter(_deviceManager.WICFactory);
+            //formatConverter.Initialize( bitmapSource, SharpDX.WIC.PixelFormat.Format32bppBGRA);
+            formatConverter.Initialize(
+                bitmapSource,
+                SharpDX.WIC.PixelFormat.Format32bppBGRA, 
+                SharpDX.WIC.BitmapDitherType.None, 
+                null, 
+                0.0f, 
+                SharpDX.WIC.BitmapPaletteType.Custom
+                ); 
+            
+            return formatConverter;
+        }
+
+
+
+        private SharpDX.Direct2D1.CustomEffect CreateEffectGraph(SharpDX.WIC.FormatConverter formatConverter, Vector2 scale)
+        {
+            // Setup local variables
+            var d2dDevice = _deviceManager.DeviceDirect2D;
+            var d2dContext = _deviceManager.ContextDirect2D;
+
+            // Effect 1 : BitmapSource - take decoded image data and get a BitmapSource from it
+            SharpDX.Direct2D1.Effects.BitmapSourceEffect bitmapSourceEffect = new SharpDX.Direct2D1.Effects.BitmapSourceEffect(d2dContext);
+            bitmapSourceEffect.ScaleSource = scale;
+            bitmapSourceEffect.WicBitmapSource = formatConverter;
+            //bitmapSourceEffect.Cached = true; // Because the image will not be changing, we should cache the effect for performance reasons.
+
+            _deviceManager.FactoryDirect2D.RegisterEffect<RippleEffect>();
+            
+
+            // Effect 2 : PointSpecular
+            _rippleEffect = new RippleEffect();
+
+            
+            return _rippleEffect;
+
+        }
+
+
+
+        private void UpdateEffectGraph()
+        {
+            
+            _effectGraph = CreateEffectGraph(
+                                _formatConverter,
+                                Scale
+                              );
+        }
+
+        private void UpdatePointer(float x, float y)
+        {
+
+            PointsAt = new Vector3(x, y, 0);
+            //_rippleEffect.LightPosition = PointsAt;
+            
+        }
+
+
+        void root_PointerMoved(object sender, Windows.UI.Xaml.Input.PointerEventArgs e)
+        {
+            var newPosition = e.GetCurrentPoint(null);
+
+            
+            var gtRoot = ((Windows.UI.Xaml.UIElement)_rootParent).TransformToVisual(_root);
+            var rootPosition = gtRoot.TransformPoint(new Windows.Foundation.Point(newPosition.Position.X, newPosition.Position.Y));
+
+
+            UpdatePointer((float)rootPosition.X, (float)rootPosition.Y);
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Samples/Win8/CustomPixelShaderEffect/RippleEffect.cs
@@ -0,0 +1,163 @@
+﻿using SharpDX.Direct2D1;
+using SharpDX;
+using System;
+using SharpDX.D3DCompiler;
+using SharpDX.IO;
+
+namespace CustomPixelShaderEffect
+{
+    class RippleEffect : CustomEffect
+    {
+
+        private Guid GUID_RipplePixelShader = Guid.NewGuid();
+
+        private DrawTransform _drawTransform;
+
+        private float _frequency;
+        public float Frequency {
+            get { return _frequency; }
+            set { _frequency = value; } 
+        }
+
+        private float _phase;
+        public float Phase
+        {
+            get { return _phase; }
+            set { _phase = value; }
+        }
+
+        private float _amplitude;
+        public float Amplitude
+        {
+            get { return _amplitude; }
+            set { _amplitude = value; }
+        }
+
+        private DrawingPointF _center;
+        public DrawingPointF Center
+        {
+            get { return _center; }
+            set { _center = value; }
+        }
+
+
+        public RippleEffect()
+        {
+            
+        }
+
+
+
+
+//        public static void Register(SharpDX.Direct2D1.Factory1 factory){
+            
+//            string pszXml = @"
+//                <?xml version='1.0'?>
+//                <Effect>
+//                    <!-- System Properties -->
+//                    <Property name='DisplayName' type='string' value='Ripple'/>
+//                    <Property name='Author' type='string' value='Microsoft Corporation'/>
+//                    <Property name='Category' type='string' value='Stylize'/>
+//                    <Property name='Description' type='string' value='Adds a ripple effect that can be animated'/>
+//
+//                    <Inputs>
+//                        <Input name='Source'/>
+//                    </Inputs>
+//
+//                    <!-- Custom Properties go here -->
+//                    <Property name='Frequency' type='float'>
+//                        <Property name='DisplayName' type='string' value='Frequency'/>
+//                        <Property name='Min' type='float' value='0.0' />
+//                        <Property name='Max' type='float' value='1000.0' />
+//                        <Property name='Default' type='float' value='0.0' />
+//                    </Property>
+//
+//                    <Property name='Phase' type='float'>
+//                        <Property name='DisplayName' type='string' value='Phase'/>
+//                        <Property name='Min' type='float' value='-100.0' />
+//                        <Property name='Max' type='float' value='100.0' />
+//                        <Property name='Default' type='float' value='0.0' />
+//                    </Property>
+//
+//                    <Property name='Amplitude' type='float'>
+//                        <Property name='DisplayName' type='string' value='Amplitude'/>
+//                        <Property name='Min' type='float' value='0.0001' />
+//                        <Property name='Max' type='float' value='1000.0' />
+//                        <Property name='Default' type='float' value='0.0' />
+//                    </Property>
+//
+//                    <Property name='Spread' type='float'>
+//                        <Property name='DisplayName' type='string' value='Spread'/>
+//                        <Property name='Min' type='float' value='0.0001' />
+//                        <Property name='Max' type='float' value='1000.0' />
+//                        <Property name='Default' type='float' value='0.0' />
+//                    </Property>
+//
+//                    <Property name='Center' type='vector2'>
+//                        <Property name='DisplayName' type='string' value='Center'/>
+//                        <Property name='Min' type='vector2' value='(-2000.0, -2000.0)' />
+//                        <Property name='Max' type='vector2' value='(2000.0, 2000.0)' />
+//                        <Property name='Default' type='vector2' value='(0.0, 0.0)' />
+//                    </Property>
+//                </Effect>
+//                ";
+
+     
+
+//        }
+
+
+
+
+
+
+        public void Initialize(EffectContext effectContext, TransformGraph transformGraph)
+        {
+            byte[] data;
+
+
+            var path = Windows.ApplicationModel.Package.Current.InstalledLocation.Path;
+
+
+            using (var stream = new NativeFileStream(path + "\\Ripple.cso", NativeFileMode.Open, NativeFileAccess.Read))
+            {
+                data = new byte[stream.Length];
+                stream.Read(data, 0, (int)stream.Length);
+            }
+
+
+
+            transformGraph.SingleTransformNode = _drawTransform;
+
+            effectContext.LoadPixelShader(
+                GUID_RipplePixelShader, data, data.Length);
+        }
+
+        public void PrepareForRender(ChangeType changeType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetGraph(TransformGraph transformGraph)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable Shadow
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
