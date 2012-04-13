dfa4277fcf372948673eab7133f52412d165176a
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2012-04-13 23:54:07 +0900

    [Direct2D1] Fix #define for non-Win8 metro compilation

--- Source/SharpDX.Direct2D1/InputDescription.cs
+++ Source/SharpDX.Direct2D1/InputDescription.cs
@@ -17,6 +17,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+#if DIRECT3D11_1
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -37,3 +38,4 @@ namespace SharpDX.Direct2D1
         }
     }
 }
+#endif
