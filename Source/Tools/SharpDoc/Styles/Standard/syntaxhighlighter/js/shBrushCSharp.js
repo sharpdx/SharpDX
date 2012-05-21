5a032592dfe0ef377f7bc22d8565a4dfdb6cc7cf
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2012-05-21 23:06:13 +0900

    [SharpDoc] Change color for strings in CoreDefault and add var keyword

--- Source/Tools/SharpDoc/Styles/Standard/syntaxhighlighter/js/shBrushCSharp.js
+++ Source/Tools/SharpDoc/Styles/Standard/syntaxhighlighter/js/shBrushCSharp.js
@@ -27,7 +27,7 @@
 						'interface internal is lock long namespace new null object operator out ' +
 						'override params private protected public readonly ref return sbyte sealed set ' +
 						'short sizeof stackalloc static string struct switch this throw true try ' +
-						'typeof uint ulong unchecked unsafe ushort using virtual void while';
+						'typeof uint ulong unchecked unsafe ushort using var virtual void while';
 
 		this.regexList = [
 			{ regex: SyntaxHighlighter.regexLib.singleLineCComments,	css: 'comments' },		// one line comments
