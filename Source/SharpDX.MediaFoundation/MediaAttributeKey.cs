2031df95fefb48c8b2e014833fbe9a030ac16008
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2012-05-29 00:03:05 +0900

    [MediaFoundation] Add Media Attributes with type information

---
+++ Source/SharpDX.MediaFoundation/MediaAttributeKey.cs
@@ -0,0 +1,191 @@
+﻿// Copyright (c) 2010-2011 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Globalization;
+
+using SharpDX.Win32;
+
+namespace SharpDX.MediaFoundation
+{
+    /// <summary>
+    /// Associate an attribute key with a type used to retreive keys from a <see cref="MediaAttributes"/> instance.
+    /// </summary>
+    public class MediaAttributeKey
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaAttributeKey"/> struct.
+        /// </summary>
+        /// <param name="guid">The GUID.</param>
+        /// <param name="type">The type.</param>
+        public MediaAttributeKey(Guid guid, Type type)
+        {
+            Guid = guid;
+            Type = type;
+        }
+
+        /// <summary>
+        /// Gets or sets the GUID.
+        /// </summary>
+        /// <value>
+        /// The GUID.
+        /// </value>
+        public Guid Guid { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public Type Type { get; private set; }
+
+
+        internal static object GetValueFromVariant(ref SharpDX.Win32.Variant variant)
+        {
+            var value = variant.Value;
+            if (value == null)
+                return null;
+
+            if (value is uint || value is int)
+            {
+                return (int)Convert.ChangeType(value, typeof(int));
+            }
+
+            if (value is ulong || value is long)
+            {
+                return (long)Convert.ChangeType(value, typeof(long));
+            }
+
+            if (value is double)
+            {
+                return (int)Convert.ChangeType(value, typeof(double));
+            }
+            
+            return value;
+        }
+
+        internal static T GetValueFromVariant<T>(ref SharpDX.Win32.Variant variant)
+        {
+            var value = variant.Value;
+            if (value == null)
+                return default(T);
+
+            if (value is uint || value is ulong || value is double)
+            {
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+
+            if (variant.Type == VariantType.Default && variant.ElementType == VariantElementType.ComUnknown && typeof(T).IsSubclassOf(typeof(ComObject)) )
+            {
+                return (T)value;
+            }
+
+            if (value.GetType() == typeof(T))
+            {
+                return (T)value;
+            }
+
+            throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "Variant value type [{0}] is not handled", value.GetType().Name));
+        }
+
+
+        internal static void SetValueToVariant<T>(ref SharpDX.Win32.Variant variant, T value)
+        {
+            if (value is int || value is bool || value.GetType().IsEnum || value is byte || value is uint)
+            {
+                variant.Value = Convert.ToUInt32(value);
+                return;
+            }
+
+            if (value is ulong || value is IntPtr || value is long)
+            {
+                variant.Value = Convert.ToUInt64(value);
+                return;
+            }
+
+            if (value is string || value is byte[] || value is ComObject)
+            {
+                variant.Value = value;
+                return;
+            }
+
+            throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "Variant value type [{0}] is not handled", value.GetType().Name));
+        }
+
+        internal static void SetValueToVariant(ref SharpDX.Win32.Variant variant, object value)
+        {
+            if (value is int || value is bool || value.GetType().IsEnum || value is byte || value is uint)
+            {
+                variant.Value = Convert.ToUInt32(value);
+                return;
+            }
+
+            if (value is ulong || value is IntPtr || value is long)
+            {
+                variant.Value = Convert.ToUInt64(value);
+                return;
+            }
+
+            if (value is string || value is byte[] || value is ComObject)
+            {
+                variant.Value = value;
+                return;
+            }
+
+            throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "Variant value type [{0}] is not handled", value.GetType().Name));
+        }
+    }
+
+
+    /// <summary>
+    /// Generic version of <see cref="MediaAttributeKey"/>
+    /// </summary>
+    /// <typeparam name="T">Type of the value of this key</typeparam>
+    public class MediaAttributeKey<T> : MediaAttributeKey
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaAttributeKey&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="guid">The GUID.</param>
+        public MediaAttributeKey(string guid)
+            : base(new Guid(guid), typeof(T))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaAttributeKey&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="guid">The GUID.</param>
+        public MediaAttributeKey(Guid guid) : base(guid, typeof(T))
+        {
+        }
+
+        internal T GetValue(ref SharpDX.Win32.Variant variant)
+        {
+            return GetValueFromVariant<T>(ref variant);
+        }
+
+        internal void SetValue<T>(ref SharpDX.Win32.Variant variant, T value)
+        {
+            SetValueToVariant(ref variant, value);
+        }
+    }
+}
--- Source/SharpDX.MediaFoundation/MediaAttributes.cs
+++ Source/SharpDX.MediaFoundation/MediaAttributes.cs
@@ -48,11 +48,11 @@ namespace SharpDX.MediaFoundation
         /// <msdn-id>ms704598</msdn-id>	
         /// <unmanaged>HRESULT IMFAttributes::GetItem([In] const GUID&amp; guidKey,[In] void* pValue)</unmanaged>	
         /// <unmanaged-short>IMFAttributes::GetItem</unmanaged-short>	
-        public unsafe object GetItem(System.Guid guidKey)
+        public unsafe object Get(System.Guid guidKey)
         {
             var value = new SharpDX.Win32.Variant();
             GetItem(guidKey, new IntPtr(&value));
-            return value.Value;
+            return MediaAttributeKey.GetValueFromVariant(ref value);
         }
 
         /// <summary>	
@@ -63,11 +63,26 @@ namespace SharpDX.MediaFoundation
         /// <msdn-id>ms704598</msdn-id>	
         /// <unmanaged>HRESULT IMFAttributes::GetItem([In] const GUID&amp; guidKey,[In] void* pValue)</unmanaged>	
         /// <unmanaged-short>IMFAttributes::GetItem</unmanaged-short>	
-        public unsafe T GetItem<T>(System.Guid guidKey)
+        public unsafe T Get<T>(System.Guid guidKey)
         {
             var value = new SharpDX.Win32.Variant();
             GetItem(guidKey, new IntPtr(&value));
-            return (T)Convert.ChangeType(value.Value, typeof(T));
+            return MediaAttributeKey.GetValueFromVariant<T>(ref value);
+        }
+
+        /// <summary>	
+        /// Gets an item value
+        /// </summary>	
+        /// <param name="guidKey">GUID of the key.</param>	
+        /// <returns>The value associated to this key.</returns>	
+        /// <msdn-id>ms704598</msdn-id>	
+        /// <unmanaged>HRESULT IMFAttributes::GetItem([In] const GUID&amp; guidKey,[In] void* pValue)</unmanaged>	
+        /// <unmanaged-short>IMFAttributes::GetItem</unmanaged-short>	
+        public unsafe T Get<T>(MediaAttributeKey<T> guidKey)
+        {
+            var variant = new SharpDX.Win32.Variant();
+            GetItem(guidKey.Guid, new IntPtr(&variant));
+            return guidKey.GetValue(ref variant);
         }
 
         /// <summary>	
@@ -81,10 +96,10 @@ namespace SharpDX.MediaFoundation
         /// <unmanaged-short>IMFAttributes::CompareItem</unmanaged-short>	
         public unsafe bool CompareItem(System.Guid guidKey, object value)
         {
-            var variant = new SharpDX.Win32.Variant { Value = value };
+            var variant = new SharpDX.Win32.Variant();
+            MediaAttributeKey.SetValueToVariant(ref variant, value);
             return CompareItem(guidKey, variant);
         }
-
 
         /// <summary>	
         /// <p><strong>Applies to: </strong>desktop apps | Metro style apps</p><p> Adds an attribute value with a specified key. </p>	
@@ -98,10 +113,49 @@ namespace SharpDX.MediaFoundation
         /// <msdn-id>bb970346</msdn-id>	
         /// <unmanaged>HRESULT IMFAttributes::SetItem([In] const GUID&amp; guidKey,[In] const PROPVARIANT&amp; Value)</unmanaged>	
         /// <unmanaged-short>IMFAttributes::SetItem</unmanaged-short>	
-        public void SetItem(System.Guid guidKey, object value)
+        public void Set(System.Guid guidKey, object value)
         {
-            var variant = new SharpDX.Win32.Variant { Value = value };
+            var variant = new SharpDX.Win32.Variant();
+            MediaAttributeKey.SetValueToVariant(ref variant, value);
             SetItem(guidKey, variant);
+        }
+
+        /// <summary>	
+        /// <p><strong>Applies to: </strong>desktop apps | Metro style apps</p><p> Adds an attribute value with a specified key. </p>	
+        /// </summary>	
+        /// <param name="guidKey"><dd> <p> A <see cref="System.Guid"/> that identifies the value to set. If this key already exists, the method overwrites the old value. </p> </dd></param>	
+        /// <param name="value"><dd> <p> A <strong><see cref="SharpDX.Win32.Variant"/></strong> that contains the attribute value. The method copies the value. The <strong><see cref="SharpDX.Win32.Variant"/></strong> type must be one of the types listed in the <strong><see cref="SharpDX.MediaFoundation.AttributeType"/></strong> enumeration. </p> </dd></param>	
+        /// <returns><p> The method returns an <strong><see cref="SharpDX.Result"/></strong>. Possible values include, but are not limited to, those in the following table. </p><table> <tr><th>Return code</th><th>Description</th></tr> <tr><td> <dl> <dt><strong><see cref="SharpDX.Result.Ok"/></strong></dt> </dl> </td><td> <p> The method succeeded. </p> </td></tr> <tr><td> <dl> <dt><strong>E_OUTOFMEMORY</strong></dt> </dl> </td><td> <p> Insufficient memory. </p> </td></tr> <tr><td> <dl> <dt><strong>MF_E_INVALIDTYPE</strong></dt> </dl> </td><td> <p> Invalid attribute type. </p> </td></tr> </table><p>?</p></returns>	
+        /// <remarks>	
+        /// <p> This method checks whether the <strong><see cref="SharpDX.Win32.Variant"/></strong> type is one of the attribute types defined in <strong><see cref="SharpDX.MediaFoundation.AttributeType"/></strong>, and fails if an unsupported type is used. However, this method does not check whether the <strong><see cref="SharpDX.Win32.Variant"/></strong> is the correct type for the specified attribute <see cref="System.Guid"/>. (There is no programmatic way to associate attribute GUIDs with property types.) For a list of Media Foundation attributes and their data types, see Media Foundation Attributes. </p><p>This interface is available on the following platforms if the Windows Media Format 11 SDK redistributable components are installed:</p><ul> <li>Windows?XP with Service Pack?2 (SP2) and later.</li> <li>Windows?XP Media Center Edition?2005 with KB900325 (Windows?XP Media Center Edition?2005) and KB925766 (October 2006 Update Rollup for Windows?XP Media Center Edition) installed.</li> </ul>	
+        /// </remarks>	
+        /// <msdn-id>bb970346</msdn-id>	
+        /// <unmanaged>HRESULT IMFAttributes::SetItem([In] const GUID&amp; guidKey,[In] const PROPVARIANT&amp; Value)</unmanaged>	
+        /// <unmanaged-short>IMFAttributes::SetItem</unmanaged-short>	
+        public unsafe void Set<T>(System.Guid guidKey, T value)
+        {
+            var variant = new SharpDX.Win32.Variant();
+            MediaAttributeKey.SetValueToVariant(ref variant, value);
+            SetItem(guidKey, variant);
+        }
+
+        /// <summary>	
+        /// <p><strong>Applies to: </strong>desktop apps | Metro style apps</p><p> Adds an attribute value with a specified key. </p>	
+        /// </summary>	
+        /// <param name="guidKey"><dd> <p> A <see cref="System.Guid"/> that identifies the value to set. If this key already exists, the method overwrites the old value. </p> </dd></param>	
+        /// <param name="value"><dd> <p> A <strong><see cref="SharpDX.Win32.Variant"/></strong> that contains the attribute value. The method copies the value. The <strong><see cref="SharpDX.Win32.Variant"/></strong> type must be one of the types listed in the <strong><see cref="SharpDX.MediaFoundation.AttributeType"/></strong> enumeration. </p> </dd></param>	
+        /// <returns><p> The method returns an <strong><see cref="SharpDX.Result"/></strong>. Possible values include, but are not limited to, those in the following table. </p><table> <tr><th>Return code</th><th>Description</th></tr> <tr><td> <dl> <dt><strong><see cref="SharpDX.Result.Ok"/></strong></dt> </dl> </td><td> <p> The method succeeded. </p> </td></tr> <tr><td> <dl> <dt><strong>E_OUTOFMEMORY</strong></dt> </dl> </td><td> <p> Insufficient memory. </p> </td></tr> <tr><td> <dl> <dt><strong>MF_E_INVALIDTYPE</strong></dt> </dl> </td><td> <p> Invalid attribute type. </p> </td></tr> </table><p>?</p></returns>	
+        /// <remarks>	
+        /// <p> This method checks whether the <strong><see cref="SharpDX.Win32.Variant"/></strong> type is one of the attribute types defined in <strong><see cref="SharpDX.MediaFoundation.AttributeType"/></strong>, and fails if an unsupported type is used. However, this method does not check whether the <strong><see cref="SharpDX.Win32.Variant"/></strong> is the correct type for the specified attribute <see cref="System.Guid"/>. (There is no programmatic way to associate attribute GUIDs with property types.) For a list of Media Foundation attributes and their data types, see Media Foundation Attributes. </p><p>This interface is available on the following platforms if the Windows Media Format 11 SDK redistributable components are installed:</p><ul> <li>Windows?XP with Service Pack?2 (SP2) and later.</li> <li>Windows?XP Media Center Edition?2005 with KB900325 (Windows?XP Media Center Edition?2005) and KB925766 (October 2006 Update Rollup for Windows?XP Media Center Edition) installed.</li> </ul>	
+        /// </remarks>	
+        /// <msdn-id>bb970346</msdn-id>	
+        /// <unmanaged>HRESULT IMFAttributes::SetItem([In] const GUID&amp; guidKey,[In] const PROPVARIANT&amp; Value)</unmanaged>	
+        /// <unmanaged-short>IMFAttributes::SetItem</unmanaged-short>	
+        public unsafe void Set<T>(MediaAttributeKey<T> guidKey, T value)
+        {
+            var variant = new SharpDX.Win32.Variant();
+            MediaAttributeKey.SetValueToVariant(ref variant, value);
+            SetItem(guidKey.Guid, variant);
         }
     }
 }
