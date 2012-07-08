0d7db3d36803623a2b927e73cc1c35cc7124da84
Alexandre Mutel <alexandre_mutel@sharpdx.org>
2012-07-08 23:57:35 +0900

    [Toolkit.Graphics] Add XNA like VertexPosition.* predefined structures

---
+++ Source/Toolkit/SharpDX.Toolkit.Graphics/VertexPositionColor.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) 2010-2012 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Runtime.InteropServices;
+using SharpDX.Direct3D11;
+
+namespace SharpDX.Toolkit.Graphics
+{
+    /// <summary>
+    /// Describes a custom vertex format structure that contains position and color information. 
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct VertexPositionColor : IVertexType, IEquatable<VertexPositionColor>
+    {
+        /// <summary>
+        /// Initializes a new <see cref="VertexPositionColor"/> instance.
+        /// </summary>
+        /// <param name="position">The position of this vertex.</param>
+        /// <param name="color">The color of this vertex.</param>
+        public VertexPositionColor(Vector3 position, Color color) : this()
+        {
+            Position = position;
+            Color = color;
+        }
+
+        /// <summary>
+        /// XYZ position.
+        /// </summary>
+        public Vector3 Position;
+
+        /// <summary>
+        /// The vertex color.
+        /// </summary>
+        public Color Color;
+
+        public InputElement[] VertexDeclaration
+        {
+            get { return InputElements; }
+        }
+
+        private static readonly InputElement[] InputElements = new[]
+                                                          {
+                                                              new InputElement("POSITION", 0, PixelFormat.R32G32B32.Float, 0),
+                                                              new InputElement("COLOR", 0, PixelFormat.R8G8B8A8.UNorm, 0),
+                                                          };
+
+        public bool Equals(VertexPositionColor other)
+        {
+            return Position.Equals(other.Position) && Color.Equals(other.Color);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is VertexPositionColor && Equals((VertexPositionColor) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Position.GetHashCode() * 397) ^ Color.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(VertexPositionColor left, VertexPositionColor right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(VertexPositionColor left, VertexPositionColor right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Position: {0}, Color: {1}", Position, Color);
+        }
+    }
+}
