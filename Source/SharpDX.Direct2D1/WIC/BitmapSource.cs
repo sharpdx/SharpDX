2481d1877fb03c47d5cea81cd850badcca0e51da
Alexandre Mutel <alexandre_mutel@sharpdx.org>
2012-07-23 07:50:49 +0900

    [WIC] Add BitmapSource.CopyPixels method with direct pointer/size

--- Source/SharpDX.Direct2D1/WIC/BitmapSource.cs
+++ Source/SharpDX.Direct2D1/WIC/BitmapSource.cs
@@ -80,6 +80,24 @@ namespace SharpDX.WIC
         /// <summary>	
         /// <p>Instructs the object to produce pixels.</p>	
         /// </summary>	
+        /// <param name="stride"><dd>  <p>The stride of the bitmap</p> </dd></param>	
+        /// <param name="dataPointer"><dd>  <p>A reference to the buffer.</p> </dd></param>	
+        /// <param name="size">Size of the buffer in bytes.</param>	
+        /// <remarks>	
+        /// <p><strong>CopyPixels</strong> is one of the two main image processing routines (the other being <strong>Lock</strong>) triggering the actual processing.  It instructs the object to produce pixels according to its algorithm - this may involve decoding a portion of a JPEG stored on disk, copying a block of memory, or even analytically computing a complex gradient.  The algorithm is completely dependent on the object implementing the interface. </p><p> The caller can restrict the operation to a rectangle of interest (ROI) using the prc parameter.  The ROI sub-rectangle must be fully contained in the bounds of the bitmap.  Specifying a <strong><c>null</c></strong> ROI implies that the whole bitmap should be returned. 	
+        /// </p><p> The caller controls the memory management and must provide an output buffer (<em>pbBuffer</em>) for the results of the copy along with the buffer's bounds (<em>cbBufferSize</em>).  The cbStride parameter defines the count of bytes between two vertically adjacent pixels in the output buffer.  The caller must ensure that there is sufficient buffer to complete the call based on the width, height and pixel format of the bitmap and the sub-rectangle provided to the copy method. </p><p> If the caller needs to perform numerous copies of an expensive <strong><see cref="SharpDX.WIC.BitmapSource"/></strong> such as a JPEG, it is recommended to create an in-memory <strong><see cref="SharpDX.WIC.Bitmap"/></strong> first. </p>Codec Developer Remarks<p> The callee must only write to the first (prc-&gt;Width*bitsperpixel+7)/8 bytes of each line of the output buffer (in this case, a line is a consecutive string of <em>cbStride</em> bytes). </p>	
+        /// </remarks>	
+        /// <msdn-id>ee690179</msdn-id>	
+        /// <unmanaged>HRESULT IWICBitmapSource::CopyPixels([In, Optional] const WICRect* prc,[In] unsigned int cbStride,[In] unsigned int cbBufferSize,[In] void* pbBuffer)</unmanaged>	
+        /// <unmanaged-short>IWICBitmapSource::CopyPixels</unmanaged-short>	
+        public void CopyPixels(int stride, IntPtr dataPointer, int size)
+        {
+            CopyPixels(null, stride, size, dataPointer);
+        }
+
+        /// <summary>	
+        /// <p>Instructs the object to produce pixels.</p>	
+        /// </summary>	
         /// <param name="rectangle"><dd>  <p>The rectangle to copy. A <strong><c>null</c></strong> value specifies the entire bitmap.</p> </dd></param>	
         /// <param name="output">The destination array. The size of the array must be sizeof(pixel) * rectangle.Width * rectangle.Height</param>	
         /// <returns><p>If this method succeeds, it returns <strong><see cref="SharpDX.Result.Ok"/></strong>. Otherwise, it returns an <strong><see cref="SharpDX.Result"/></strong> error code.</p></returns>	
