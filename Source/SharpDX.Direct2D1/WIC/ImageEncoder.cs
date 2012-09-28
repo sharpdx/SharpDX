453bb00a589e20a3c141b5718838febbfc842e5e
Alexandre Mutel <alexandre_mutel@sharpdx.org>
2012-09-28 21:07:53 +0900

    [Direct2D1] Add ImageEncoder constructor, remove previous ImagingFactor2.CreateImageEncoder method. Tranform WriteFrame methods using value-type for ImageParameters.

---
+++ Source/SharpDX.Direct2D1/WIC/ImageEncoder.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2010-2012 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+#if DIRECTX11_1
+
+namespace SharpDX.WIC
+{
+    public partial class ImageEncoder
+    {
+        /// <summary>
+        /// Creates a new image encoder object.
+        /// </summary>
+        /// <param name="factory">The WIC factory.</param>
+        /// <param name="d2dDevice">The <see cref="Direct2D1.Device" /> object on which the corresponding image encoder is created.</param>
+        /// <msdn-id>hh880849</msdn-id>
+        ///   <unmanaged>HRESULT IWICImagingFactory2::CreateImageEncoder([In] ID2D1Device* pD2DDevice,[In] IWICImageEncoder** ppWICImageEncoder)</unmanaged>
+        ///   <unmanaged-short>IWICImagingFactory2::CreateImageEncoder</unmanaged-short>
+        public ImageEncoder(ImagingFactory2 factory, Direct2D1.Device d2dDevice)
+        {
+            factory.CreateImageEncoder(d2dDevice, this);
+        }
+    }
+}
+#endif
---
+++ Source/SharpDX.Direct2D1/WIC/ImageParameters.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2010-2012 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+#if DIRECTX11_1
+
+namespace SharpDX.WIC
+{
+    public partial struct ImageParameters
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImageParameters" /> struct.
+        /// </summary>
+        /// <param name="pixelFormat">The pixel format.</param>
+        /// <param name="dpiX">The dpi X.</param>
+        /// <param name="dpiY">The dpi Y.</param>
+        /// <param name="top">The top.</param>
+        /// <param name="left">The left.</param>
+        /// <param name="pixelWidth">Width in pixel.</param>
+        /// <param name="pixelHeight">Height in pixel.</param>
+        public ImageParameters(Direct2D1.PixelFormat pixelFormat, float dpiX, float dpiY, float top, float left, int pixelWidth, int pixelHeight)
+        {
+            PixelFormat = pixelFormat;
+            DpiX = dpiX;
+            DpiY = dpiY;
+            Top = top;
+            Left = left;
+            PixelWidth = pixelWidth;
+            PixelHeight = pixelHeight;
+        }
+    }
+}
+#endif
