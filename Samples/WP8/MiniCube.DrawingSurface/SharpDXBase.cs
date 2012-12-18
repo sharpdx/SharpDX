59f44831500a4b7fbf797bfb2a3556427e434e6d
jf <fajardo_jf@hotmail.com>
2012-12-19 03:13:13 +1100

    [WP8 Sample] Sample for IDrawingSurfaceContentProvider
    Start work on a sample to test out the new
    IDrawingSurfaceContentProvider

---
+++ Samples/WP8/MiniCube.DrawingSurface/SharpDXBase.cs
@@ -0,0 +1,140 @@
+﻿// Copyright (c) 2010-2012 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using SharpDX;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+
+namespace MiniTriApp
+{
+    /// <summary>
+    /// Helper class that initializes SharpDX APIs for 3D rendering.
+    /// This is a port of Direct3D C++ WP8 sample. This port is not clean and complete. 
+    /// The preferred way to access Direct3D on WP8 is by using SharpDX.Toolkit.
+    /// </summary>
+    internal abstract class SharpDXBase : Component
+    {
+        // Constructor.
+        internal SharpDXBase()
+        {
+        }
+
+        public void Update(Device device, DeviceContext context, RenderTargetView renderTargetView)
+        {
+            if (device != _device)
+            {
+                _device = device;
+                CreateDeviceResources();
+            }
+
+            _deviceContext = context;
+            _renderTargetview = renderTargetView;
+
+            CreateWindowSizeDependentResources();
+        }
+
+        public virtual void CreateDeviceResources()
+        {
+        }
+
+        public virtual void CreateWindowSizeDependentResources()
+        {
+            var resource = _renderTargetview.Resource;
+            using (var texture2D = new Texture2D(resource.NativePointer))
+            {
+
+                var currentWidth = (int) _renderTargetSize.Width;
+                var currentHeight = (int) _renderTargetSize.Height;
+
+                if (currentWidth != texture2D.Description.Width &&
+                    currentHeight != texture2D.Description.Height)
+                {
+                    _renderTargetSize.Width = texture2D.Description.Width;
+                    _renderTargetSize.Height = texture2D.Description.Height;
+
+                    ComObject.Dispose(ref _depthStencilView);
+
+                    using (var depthTexture = new Texture2D(
+                        _device,
+                        new Texture2DDescription()
+                            {
+                                Width = (int) _renderTargetSize.Width,
+                                Height = (int) _renderTargetSize.Height,
+                                ArraySize = 1,
+                                BindFlags = BindFlags.DepthStencil,
+                                CpuAccessFlags = CpuAccessFlags.None,
+                                Format = SharpDX.DXGI.Format.D24_UNorm_S8_UInt,
+                                MipLevels = 1,
+                                OptionFlags = ResourceOptionFlags.None,
+                                SampleDescription = new SharpDX.DXGI.SampleDescription(1, 0),
+                                Usage = ResourceUsage.Default
+                            }))
+                        _depthStencilView = new DepthStencilView(_device, depthTexture);
+                }
+            }
+
+            _windowBounds.Width = _renderTargetSize.Width;
+            _windowBounds.Height = _renderTargetSize.Height;
+
+            // Create a viewport descriptor of the full window size.
+             var viewport = new SharpDX.Direct3D11.Viewport(0, 0, (float)_renderTargetSize.Width, (float)_renderTargetSize.Height );
+
+            _deviceContext.Rasterizer.SetViewports(viewport);
+        }
+
+        public virtual void UpdateForWindowSizeChange(float width, float height)
+        {
+	        _renderTargetSize.Width = width;
+	        _renderTargetSize.Height = height;
+
+	        RenderTargetView[] nullViews = {null};
+	        //_deviceContext.SetRenderTargets(ARRAYSIZE(nullViews), nullViews, null);
+	        _renderTarget = null;
+	        _renderTargetview = null;
+	        _depthStencilView = null;
+	        _deviceContext.Flush();
+
+	        CreateWindowSizeDependentResources();
+        }
+
+        public abstract void Render();
+
+        internal virtual Texture2D GetTexture()
+        {
+            return _renderTarget;
+        }
+
+        // Direct3D Objects.
+        protected Device _device;
+        protected DeviceContext _deviceContext;
+        protected Texture2D _renderTarget;
+        protected RenderTargetView _renderTargetview;
+        protected DepthStencilView _depthStencilView;
+
+        // Cached renderer properties.
+        protected FeatureLevel _featureLevel;
+        protected Windows.Foundation.Size _renderTargetSize;
+        protected Windows.Foundation.Rect _windowBounds;
+    }
+}
---
+++ Samples/WP8/MiniCube.DrawingSurface/SharpDXInterop.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (c) 2010-2012 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+using SharpDX.Direct3D11;
+
+namespace MiniTriApp
+{
+    /// <summary>
+    /// This is a port of Direct3D C++ WP8 sample. This port is not clean and complete. 
+    /// The preferred way to access Direct3D on WP8 is by using SharpDX.Toolkit.
+    /// </summary>
+    internal class SharpDXInterop : Windows.Phone.Input.Interop.IDrawingSurfaceManipulationHandler
+    {
+        public SharpDXInterop()
+        {
+            _timer = new BasicTimer();
+        }
+
+        internal object CreateContentProvider()
+        {
+	        var provider =  new SharpDXContentProvider(this);
+            return provider;
+        }
+
+        // IDrawingSurfaceManipulationHandler
+        public void SetManipulationHost(Windows.Phone.Input.Interop.DrawingSurfaceManipulationHost manipulationHost)
+        {
+            manipulationHost.PointerPressed += OnPointerPressed;
+
+            manipulationHost.PointerMoved += OnPointerMoved;
+
+            manipulationHost.PointerReleased += OnPointerReleased;
+        }
+
+        // Event Handlers
+        protected void OnPointerPressed(Windows.Phone.Input.Interop.DrawingSurfaceManipulationHost sender, Windows.UI.Core.PointerEventArgs args)
+        {
+            // Insert your code here.
+        }
+
+        protected void OnPointerMoved(Windows.Phone.Input.Interop.DrawingSurfaceManipulationHost sender, Windows.UI.Core.PointerEventArgs args)
+        {
+            // Insert your code here.
+        }
+
+        protected void OnPointerReleased(Windows.Phone.Input.Interop.DrawingSurfaceManipulationHost sender, Windows.UI.Core.PointerEventArgs args)
+        {
+            // Insert your code here.
+        }
+
+        internal void Connect()
+        {
+	        _renderer = new CubeRenderer();
+
+	        // Restart timer after renderer has finished initializing.
+	        _timer.Reset();
+        }
+
+        internal void Disconnect()
+        {
+            _renderer = null;
+        }
+
+        internal void UpdateForWindowSizeChange(float width, float height)
+        {
+            _renderer.UpdateForWindowSizeChange(width, height);
+        }
+
+        internal void Render(Device device, DeviceContext context, RenderTargetView renderTargetView)
+        {
+            _timer.Update();
+            _renderer.Update(device, context, renderTargetView);
+            _renderer.Render();
+        }
+
+        internal Texture2D GetTexture()
+        {
+            return _renderer.GetTexture();
+        }
+
+        private CubeRenderer _renderer;
+        private readonly BasicTimer _timer;
+
+    }
+}
