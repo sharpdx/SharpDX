099ae8877fd4747f1cce30826f82279976f1e3e1
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2012-12-24 13:35:07 +0900

    [SharpDX] Fix comments

--- Source/SharpDX/Half.cs
+++ Source/SharpDX/Half.cs
@@ -27,7 +27,7 @@ using SharpDX.Serialization;
 namespace SharpDX
 {
     /// <summary>
-    ///   A half precision (16 bit) floating point value.
+    /// A half precision (16 bit) floating point value.
     /// </summary>
     [StructLayout(LayoutKind.Sequential)]
 #if !W8CORE
@@ -40,63 +40,63 @@ namespace SharpDX
         private ushort value;
 
         /// <summary>
-        ///   Number of decimal digits of precision.
+        /// Number of decimal digits of precision.
         /// </summary>
         public const int PrecisionDigits = 3;
 
         /// <summary>
-        ///   Number of bits in the mantissa.
+        /// Number of bits in the mantissa.
         /// </summary>
         public const int MantissaBits = 11;
 
         /// <summary>
-        ///   Maximum decimal exponent.
+        /// Maximum decimal exponent.
         /// </summary>
         public const int MaximumDecimalExponent = 4;
 
         /// <summary>
-        ///   Maximum binary exponent.
+        /// Maximum binary exponent.
         /// </summary>
         public const int MaximumBinaryExponent = 15;
 
         /// <summary>
-        ///   Minimum decimal exponent.
+        /// Minimum decimal exponent.
         /// </summary>
         public const int MinimumDecimalExponent = -4;
 
         /// <summary>
-        ///   Minimum binary exponent.
+        /// Minimum binary exponent.
         /// </summary>
         public const int MinimumBinaryExponent = -14;
 
         /// <summary>
-        ///   Exponent radix.
+        /// Exponent radix.
         /// </summary>
         public const int ExponentRadix = 2;
 
         /// <summary>
-        ///   Additional rounding.
+        /// Additional rounding.
         /// </summary>
         public const int AdditionRounding = 1;
 
         /// <summary>
-        ///   Smallest such that 1.0 + epsilon != 1.0
+        /// Smallest such that 1.0 + epsilon != 1.0
         /// </summary>
         public static readonly float Epsilon;
 
         /// <summary>
-        ///   Maximum value of the number.
+        /// Maximum value of the number.
         /// </summary>
         public static readonly float MaxValue;
 
         /// <summary>
-        ///   Minimum value of the number.
+        /// Minimum value of the number.
         /// </summary>
         public static readonly float MinValue;
 
 
         /// <summary>
-        ///   Initializes a new instance of the <see cref = "T:SharpDX.Half" /> structure.
+        /// Initializes a new instance of the <see cref = "T:SharpDX.Half" /> structure.
         /// </summary>
         /// <param name = "value">The floating point value that should be stored in 16 bit format.</param>
         public Half(float value)
@@ -105,7 +105,7 @@ namespace SharpDX
         }
 
         /// <summary>
-        ///   Gets or sets the raw 16 bit value used to back this half-float.
+        /// Gets or sets the raw 16 bit value used to back this half-float.
         /// </summary>
         public ushort RawValue
         {
@@ -114,7 +114,7 @@ namespace SharpDX
         }
 
         /// <summary>
-        ///   Converts an array of half precision values into full precision values.
+        /// Converts an array of half precision values into full precision values.
         /// </summary>
         /// <param name = "values">The values to be converted.</param>
         /// <returns>An array of converted values.</returns>
@@ -127,7 +127,7 @@ namespace SharpDX
         }
 
         /// <summary>
-        ///   Converts an array of full precision values into half precision values.
+        /// Converts an array of full precision values into half precision values.
         /// </summary>
         /// <param name = "values">The values to be converted.</param>
         /// <returns>An array of converted values.</returns>
@@ -140,7 +140,7 @@ namespace SharpDX
         }
 
         /// <summary>
-        ///   Performs an explicit conversion from <see cref = "T:System.Single" /> to <see cref = "T:SharpDX.Half" />.
+        /// Performs an explicit conversion from <see cref = "T:System.Single" /> to <see cref = "T:SharpDX.Half" />.
         /// </summary>
         /// <param name = "value">The value to be converted.</param>
         /// <returns>The converted value.</returns>
@@ -150,7 +150,7 @@ namespace SharpDX
         }
 
         /// <summary>
-        ///   Performs an implicit conversion from <see cref = "T:SharpDX.Half" /> to <see cref = "T:System.Single" />.
+        /// Performs an implicit conversion from <see cref = "T:SharpDX.Half" /> to <see cref = "T:System.Single" />.
         /// </summary>
         /// <param name = "value">The value to be converted.</param>
         /// <returns>The converted value.</returns>
@@ -160,31 +160,31 @@ namespace SharpDX
         }
 
         /// <summary>
-        ///   Tests for equality between two objects.
+        /// Tests for equality between two objects.
         /// </summary>
         /// <param name = "left">The first value to compare.</param>
         /// <param name = "right">The second value to compare.</param>
         /// <returns>
-        ///   <c>true</c> if <paramref name = "left" /> has the same value as <paramref name = "right" />; otherwise, <c>false</c>.</returns>
+        /// <c>true</c> if <paramref name = "left" /> has the same value as <paramref name = "right" />; otherwise, <c>false</c>.</returns>
         public static bool operator ==(Half left, Half right)
         {
             return left.value == right.value;
         }
 
         /// <summary>
-        ///   Tests for inequality between two objects.
+        /// Tests for inequality between two objects.
         /// </summary>
         /// <param name = "left">The first value to compare.</param>
         /// <param name = "right">The second value to compare.</param>
         /// <returns>
-        ///   <c>true</c> if <paramref name = "left" /> has a different value than <paramref name = "right" />; otherwise, <c>false</c>.</returns>
+        /// <c>true</c> if <paramref name = "left" /> has a different value than <paramref name = "right" />; otherwise, <c>false</c>.</returns>
         public static bool operator !=(Half left, Half right)
         {
             return left.value != right.value;
         }
 
         /// <summary>
-        ///   Converts the value of the object to its equivalent string representation.
+        /// Converts the value of the object to its equivalent string representation.
         /// </summary>
         /// <returns>The string representation of the value of this instance.</returns>
         public override string ToString()
@@ -194,7 +194,7 @@ namespace SharpDX
         }
 
         /// <summary>
-        ///   Returns the hash code for this instance.
+        /// Returns the hash code for this instance.
         /// </summary>
         /// <returns>A 32-bit signed integer hash code.</returns>
         public override int GetHashCode()
@@ -218,35 +218,35 @@ namespace SharpDX
         }
 
         /// <summary>
-        ///   Determines whether the specified object instances are considered equal.
+        /// Determines whether the specified object instances are considered equal.
         /// </summary>
         /// <param name = "value1" />
         /// <param name = "value2" />
         /// <returns>
-        ///   <c>true</c> if <paramref name = "value1" /> is the same instance as <paramref name = "value2" /> or 
-        ///   if both are <c>null</c> references or if <c>value1.Equals(value2)</c> returns <c>true</c>; otherwise, <c>false</c>.</returns>
+        /// <c>true</c> if <paramref name = "value1" /> is the same instance as <paramref name = "value2" /> or 
+        /// if both are <c>null</c> references or if <c>value1.Equals(value2)</c> returns <c>true</c>; otherwise, <c>false</c>.</returns>
         public static bool Equals(ref Half value1, ref Half value2)
         {
             return value1.value == value2.value;
         }
 
         /// <summary>
-        ///   Returns a value that indicates whether the current instance is equal to the specified object.
+        /// Returns a value that indicates whether the current instance is equal to the specified object.
         /// </summary>
         /// <param name = "other">Object to make the comparison with.</param>
         /// <returns>
-        ///   <c>true</c> if the current instance is equal to the specified object; <c>false</c> otherwise.</returns>
+        /// <c>true</c> if the current instance is equal to the specified object; <c>false</c> otherwise.</returns>
         public bool Equals(Half other)
         {
             return other.value == value;
         }
 
         /// <summary>
-        ///   Returns a value that indicates whether the current instance is equal to a specified object.
+        /// Returns a value that indicates whether the current instance is equal to a specified object.
         /// </summary>
         /// <param name = "obj">Object to make the comparison with.</param>
         /// <returns>
-        ///   <c>true</c> if the current instance is equal to the specified object; <c>false</c> otherwise.</returns>
+        /// <c>true</c> if the current instance is equal to the specified object; <c>false</c> otherwise.</returns>
         public override bool Equals(object obj)
         {
             if (obj == null)
