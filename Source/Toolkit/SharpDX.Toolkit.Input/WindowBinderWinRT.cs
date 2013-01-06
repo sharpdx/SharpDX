df6d4e9ac8236eff19bbff6a532b3892e3059951
ArtiomCiumac <artiom.ciumac@gmail.com>
2013-01-06 18:31:42 +0200

    fixed csproj and fixed WinRT xaml integration

--- Source/Toolkit/SharpDX.Toolkit.Input/WindowBinderWinRT.cs
+++ Source/Toolkit/SharpDX.Toolkit.Input/WindowBinderWinRT.cs
@@ -26,10 +26,11 @@ using Windows.Foundation;
 using Windows.Graphics.Display;
 using Windows.UI.Core;
 using Windows.UI.Input;
+using Windows.UI.Xaml;
 
 namespace SharpDX.Toolkit.Input
 {
-
+    using global::Windows.UI.Xaml.Input;
 
     /// <summary>
     /// Represents a specific <see cref="WindowBinder"/> implementation for the WinRT platform
@@ -43,30 +44,43 @@ namespace SharpDX.Toolkit.Input
         /// <summary>
         /// Initializes a new instance of <see cref="WindowBinderWinRT"/> class.
         /// </summary>
-        /// <param name="nativeWindow">A reference to <see cref="CoreWindow"/> class.</param>
+        /// <param name="nativeWindow">A reference to <see cref="CoreWindow"/> or <see cref="UIElement"/> class.</param>
         internal WindowBinderWinRT(object nativeWindow) : base(nativeWindow) { }
 
         /// <summary>
         /// Binds to specific events of the provided CoreWindow
         /// </summary>
-        /// <param name="nativeWindow">A reference to <see cref="CoreWindow"/> class.</param>
+        /// <param name="nativeWindow">A reference to <see cref="CoreWindow"/> or <see cref="UIElement"/> class.</param>
         /// <exception cref="ArgumentNullException">Is thrown when <paramref name="nativeWindow"/> is null.</exception>
         /// <exception cref="InvalidCastException">Is thrown when <paramref name="nativeWindow"/> is not an instance of the <see cref="CoreWindow"/> class.</exception>
         protected override void BindWindow(object nativeWindow)
         {
             if (nativeWindow == null) throw new ArgumentNullException("nativeWindow");
-            var w = (CoreWindow)nativeWindow;
-
-            w.PointerPressed += HandlePointerPressed;
-            w.PointerReleased += HandlePointerReleased;
-            w.PointerWheelChanged += HandlePointerWheelChanged;
-            w.PointerMoved += HandlePointerMoved;
+            var window = nativeWindow as CoreWindow;
+            if (window != null)
+            {
+                window.PointerPressed += HandleWindowPointerEvent;
+                window.PointerReleased += HandleWindowPointerEvent;
+                window.PointerWheelChanged += HandleWindowPointerEvent;
+                window.PointerMoved += HandleWindowPointerEvent;
+            }
+            else
+            {
+                var uiElement = nativeWindow as UIElement;
+                if (uiElement != null)
+                {
+                    uiElement.PointerPressed += HandleUIElementPointerEvent;
+                    uiElement.PointerReleased += HandleUIElementPointerEvent;
+                    uiElement.PointerWheelChanged += HandleUIElementPointerEvent;
+                    uiElement.PointerMoved += HandleUIElementPointerEvent;
+                }
+            }
         }
 
         /// <summary>
         /// Returns the mouse cursor position from cached values
         /// </summary>
-        /// <param name="nativeWindow">A reference to <see cref="CoreWindow"/> class.</param>
+        /// <param name="nativeWindow">A reference to <see cref="CoreWindow"/> or <see cref="UIElement"/> class.</param>
         /// <returns>The location of mouse cursor</returns>
         protected override Point GetLocationInternal(object nativeWindow)
         {
@@ -74,11 +88,11 @@ namespace SharpDX.Toolkit.Input
         }
 
         /// <summary>
-        /// Handles the <see cref="CoreWindow.PointerWheelChanged"/> event
+        /// Handles the <see cref="CoreWindow.PointerPressed"/>, <see cref="CoreWindow.PointerReleased"/>, <see cref="CoreWindow.PointerWheelChanged"/> and <see cref="CoreWindow.PointerMoved"/> events
         /// </summary>
         /// <param name="sender">Sender widow. Not used</param>
         /// <param name="args">Pointer event arguments.</param>
-        private void HandlePointerWheelChanged(CoreWindow sender, PointerEventArgs args)
+        private void HandleWindowPointerEvent(CoreWindow sender, PointerEventArgs args)
         {
             var p = args.CurrentPoint;
             // if the current device is not a mouse - ignore it
@@ -90,45 +104,18 @@ namespace SharpDX.Toolkit.Input
         }
 
         /// <summary>
-        /// Handles the <see cref="CoreWindow.PointerReleased"/> event
+        /// Handles the <see cref="UIElement.PointerPressed"/>, <see cref="UIElement.PointerReleased"/>, <see cref="UIElement.PointerWheelChanged"/> and <see cref="UIElement.PointerMoved"/> events
         /// </summary>
-        /// <param name="sender">Sender widow. Not used</param>
+        /// <param name="sender">Sender element.</param>
         /// <param name="args">Pointer event arguments.</param>
-        private void HandlePointerReleased(CoreWindow sender, PointerEventArgs args)
+        /// <exception cref="ArgumentNullException">Is thrown if either <paramref name="sender"/>  or <paramref name="args"/> are null</exception>
+        /// <exception cref="InvalidCastException">Is thrown if <paramref name="sender"/> is not an <see cref="UIElement"/></exception>
+        private void HandleUIElementPointerEvent(object sender, PointerRoutedEventArgs args)
         {
-            var p = args.CurrentPoint;
-            // if the current device is not a mouse - ignore it
-            if (p.PointerDevice.PointerDeviceType != PointerDeviceType.Mouse) return;
+            if(sender == null) throw new ArgumentNullException("sender");
+            if(args == null) throw new ArgumentNullException("args");
 
-            UpdateMouse(p);
-
-            args.Handled = true;
-        }
-
-        /// <summary>
-        /// Handles the <see cref="CoreWindow.PointerPressed"/> event
-        /// </summary>
-        /// <param name="sender">Sender widow. Not used</param>
-        /// <param name="args">Pointer event arguments.</param>
-        private void HandlePointerPressed(CoreWindow sender, PointerEventArgs args)
-        {
-            var p = args.CurrentPoint;
-            // if the current device is not a mouse - ignore it
-            if (p.PointerDevice.PointerDeviceType != PointerDeviceType.Mouse) return;
-
-            UpdateMouse(p);
-
-            args.Handled = true;
-        }
-
-        /// <summary>
-        /// Handles the <see cref="CoreWindow.PointerMoved"/> event
-        /// </summary>
-        /// <param name="sender">Sender widow. Not used</param>
-        /// <param name="args">Pointer event arguments.</param>
-        private void HandlePointerMoved(CoreWindow sender, PointerEventArgs args)
-        {
-            var p = args.CurrentPoint;
+            var p = args.GetCurrentPoint((UIElement)sender);
             // if the current device is not a mouse - ignore it
             if (p.PointerDevice.PointerDeviceType != PointerDeviceType.Mouse) return;
 
