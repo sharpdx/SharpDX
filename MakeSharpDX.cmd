1785c7eee6972499fc2eee5bf1ce3101f14f4158
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2013-02-03 08:38:05 +0900

    [Build] Start to migrate from Nant to msbuild for building/packaging

--- MakeSharpDX.cmd
+++ MakeSharpDX.cmd
@@ -4,4 +4,4 @@ set PATH=C:\Program Files (x86)\Git\bin\;%PATH%
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\vc\vcvarsall.bat" x86
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\vc\vcvarsall.bat" x86
 set DXROOT=C:\Program Files\Sandcastle
-%~dp0\External\nant\bin\nant %*
+%~dp0\External\nant\bin\nant -buildfile:SharpDXNant.build %*
