bf3d917a4bece9e47a0683dfc814443f65f1320a
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2013-02-20 23:08:02 +0900

    [SharpDX] Fix issue #380. Add ObjectTracker Tracked/UnTracked events.

--- Source/SharpDX/Diagnostics/ObjectTracker.cs
+++ Source/SharpDX/Diagnostics/ObjectTracker.cs
@@ -27,10 +27,20 @@ using SharpDX.Collections;
 
 namespace SharpDX.Diagnostics
 {
+    /// <summary>
+    /// Event args for <see cref="ComObject"/> used by <see cref="ObjectTracker"/>.
+    /// </summary>
     public class ComObjectEventArgs : EventArgs
     {
+        /// <summary>
+        /// The object being tracked/untracked.
+        /// </summary>
         public ComObject Object;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ComObjectEventArgs"/> class.
+        /// </summary>
+        /// <param name="o">The o.</param>
         public ComObjectEventArgs(ComObject o)
         {
             Object = o;
