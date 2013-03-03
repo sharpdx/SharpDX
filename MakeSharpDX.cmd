a897d42bde7c8991a4a223b699e8132c4d7073be
Alexandre Mutel <alexandre_mutel@yahoo.fr>
2013-03-04 06:57:05 +0900

    [Build] Start to switch to new build system on CLI

--- MakeSharpDX.cmd
+++ MakeSharpDX.cmd
@@ -3,5 +3,4 @@ setlocal
 set PATH=C:\Program Files (x86)\Git\bin\;%PATH%
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\vc\vcvarsall.bat" x86
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\vc\vcvarsall.bat" x86
-set DXROOT=C:\Program Files\Sandcastle
-%~dp0\External\nant\bin\nant -buildfile:SharpDXNant.build %*
+msbuild /tv:4.0 %* SharpDX.build
