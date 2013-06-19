3211c7a93a93c13185b3c2bf6d278e3501cbf473
Steve Hoelzer <shoelzer@gmail.com>
2013-06-19 14:00:00 -0500

    [Animation] Spell check

--- Source/SharpDX.Animation/PriorityComparisonShadow.cs
+++ Source/SharpDX.Animation/PriorityComparisonShadow.cs
@@ -32,7 +32,7 @@ namespace SharpDX.Animation
         public Manager.PriorityComparisonDelegate Delegate;
 
         /// <summary>
-        /// Return a pointer to the unamanged version of this callback.
+        /// Return a pointer to the unmanaged version of this callback.
         /// </summary>
         /// <param name="callback">The callback.</param>
         /// <returns>A pointer to a shadow c++ callback</returns>
@@ -84,7 +84,7 @@ namespace SharpDX.Animation
 
         protected override void Dispose(bool disposing)
         {
-            // Releae the shadow
+            // Release the shadow
             if (disposing)
             {
                 if (Shadow != null)
