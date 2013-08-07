262c28d7ef99c8e5d905c3def872f4c78dfa8696
ArtiomCiumac <artiom.ciumac@gmail.com>
2013-08-07 11:25:25 +0300

    [SharpDX.Tests] fixed some test cases for MathUtilWrapTests

--- Source/Tests/SharpDX.Tests/MathUtilWrapTests.cs
+++ Source/Tests/SharpDX.Tests/MathUtilWrapTests.cs
@@ -13,7 +13,7 @@
         [TestCase(0, 0, 10, 0)]
         [TestCase(10, 0, 15, 5)]
         [TestCase(-10, -1, -15, -6)]
-        [TestCase(-1, -10, -15, -5)]
+        [TestCase(-1, -10, -15, -4)]
         public void WrapsInt(int min, int max, int valueToWrap, int expectedResult)
         {
             var result = MathUtil.Wrap(valueToWrap, min, max);
@@ -29,12 +29,12 @@
         [TestCase(0f, 0f, 1.0f, 0f)]
         [TestCase(1.0f, 0f, 1.5f, 0.5f)]
         [TestCase(-1.0f, -0.1f, -1.5f, -0.6f)]
-        [TestCase(-0.1f, -1.0f, -1.5f, -0.5f)]
+        [TestCase(-0.1f, -1.0f, -1.5f, -0.4f)]
         public void WrapsFloat(float min, float max, float valueToWrap, float expectedResult)
         {
             var result = MathUtil.Wrap(valueToWrap, min, max);
 
-            Assert.AreEqual(expectedResult, result);
+            Assert.That(MathUtil.WithinEpsilon(expectedResult, result, float.Epsilon), string.Format("Expected {0} but was {1}", expectedResult, result));
         }
     }
 }
