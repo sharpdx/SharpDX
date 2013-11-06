2d8e416620bb72e0aafec4dd2cad0add1e8bed40
ArtiomCiumac <artiom.ciumac@gmail.com>
2013-11-06 19:41:48 +0200

    [SharpDX] Removed unused struct as it represents the Ansi version, while only Unicode one is used (issue #190).

--- Source/SharpDX/Win32Native.cs
+++ Source/SharpDX/Win32Native.cs
@@ -73,30 +73,6 @@ namespace SharpDX
             public byte tmCharSet;
         }
 
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-        public struct TextMetricA
-        {
-            public int tmHeight;
-            public int tmAscent;
-            public int tmDescent;
-            public int tmInternalLeading;
-            public int tmExternalLeading;
-            public int tmAveCharWidth;
-            public int tmMaxCharWidth;
-            public int tmWeight;
-            public int tmOverhang;
-            public int tmDigitizedAspectX;
-            public int tmDigitizedAspectY;
-            public byte tmFirstChar;
-            public byte tmLastChar;
-            public byte tmDefaultChar;
-            public byte tmBreakChar;
-            public byte tmItalic;
-            public byte tmUnderlined;
-            public byte tmStruckOut;
-            public byte tmPitchAndFamily;
-            public byte tmCharSet;
-        }
 #if !W8CORE
 
         [StructLayout(LayoutKind.Sequential)]
