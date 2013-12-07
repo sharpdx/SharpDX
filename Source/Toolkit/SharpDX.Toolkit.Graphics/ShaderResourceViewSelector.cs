9eb673b34d969f48e04938320d62008a0970d9e8
Alexandre Mutel <alexandre_mutel@live.com>
2013-12-08 08:16:18 +0900

    [Toolkit.Graphics] Fix compilation issue

--- Source/Toolkit/SharpDX.Toolkit.Graphics/ShaderResourceViewSelector.cs
+++ Source/Toolkit/SharpDX.Toolkit.Graphics/ShaderResourceViewSelector.cs
@@ -18,7 +18,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System;
 using SharpDX.Direct3D11;
+using SharpDX.DXGI;
 
 namespace SharpDX.Toolkit.Graphics
 {
@@ -32,6 +34,25 @@ namespace SharpDX.Toolkit.Graphics
         internal ShaderResourceViewSelector(Texture thisTexture)
         {
             this.texture = thisTexture;
+        }
+
+        /// <summary>
+        /// Gets a specific <see cref="ShaderResourceView" /> from this texture.
+        /// </summary>
+        /// <param name="viewType">Type of the view.</param>
+        /// <param name="arrayOrDepthSlice">The array or depth slice.</param>
+        /// <param name="mipIndex">Index of the mip.</param>
+        /// <returns>An <see cref="ShaderResourceView" /></returns>
+        public TextureView this[ViewType viewType, int arrayOrDepthSlice, int mipIndex]
+        {
+            get
+            {
+                if(FormatHelper.IsTypeless(texture.Format))
+                {
+                    throw new InvalidOperationException(string.Format("Cannot create a SRV on a TypeLess texture format [{0}]", texture.Format));
+                }
+                return texture.GetShaderResourceView(texture.Format, viewType, arrayOrDepthSlice, mipIndex);
+            }
         }
 
         /// <summary>
