be1ac912ea6f2e6a98394bb598ebe01e567d46b0
ArtiomCiumac <artiom.ciumac@gmail.com>
2014-01-21 15:33:56 +0200

    [Build] Reverted local rebuild to use VS2012 environment instead of VS2013 as this breaks WP8 platform build.

--- MakeSharpDX_localbuild.cmd
+++ MakeSharpDX_localbuild.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set PATH=C:\Program Files (x86)\Git\bin\;%PATH%
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\vc\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\vc\vcvarsall.bat" x86
 msbuild /tv:4.0 /p:BuildSignedSharpDX=false /t:Build;BuildSamples /verbosity:quiet /clp:ErrorsOnly /fl /flp:logfile=BuildErrors.log;ErrorsOnly %* SharpDX.build
 if NOT ERRORLEVEL 0 {
 	pause
