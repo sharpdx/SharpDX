be998eef93c3d07dadbb5cc65305775dd150c947
Alexandre Mutel <alexandre_mutel@live.com>
2014-02-05 20:33:14 +0900

    [Toolkit.Graphics] Add GeometricPrimitive.Plane uv factor for uv remapping

--- Source/Toolkit/SharpDX.Toolkit.Graphics/GeometricPrimitive.Plane.cs
+++ Source/Toolkit/SharpDX.Toolkit.Graphics/GeometricPrimitive.Plane.cs
@@ -41,6 +41,24 @@ namespace SharpDX.Toolkit.Graphics
             /// <exception cref="System.ArgumentOutOfRangeException">tessellation;tessellation must be > 0</exception>
             public static GeometricPrimitive New(GraphicsDevice device, float sizeX = 1.0f, float sizeY = 1.0f, int tessellation = 1, bool toLeftHanded = false)
             {
+                return New(device, sizeX, sizeY, tessellation, Vector2.One, toLeftHanded);
+
+
+            }
+
+            /// <summary>
+            /// Creates a Plane primitive on the X/Y plane with a normal equal to -<see cref="Vector3.UnitZ" />.
+            /// </summary>
+            /// <param name="device">The device.</param>
+            /// <param name="sizeX">The size X.</param>
+            /// <param name="sizeY">The size Y.</param>
+            /// <param name="tessellation">The tessellation, as the number of quads per axis.</param>
+            /// <param name="uvFactor">The uv factor.</param>
+            /// <param name="toLeftHanded">if set to <c>true</c> vertices and indices will be transformed to left handed. Default is true.</param>
+            /// <returns>A Plane primitive.</returns>
+            /// <exception cref="System.ArgumentOutOfRangeException">tessellation;tessellation must be &gt; 0</exception>
+            public static GeometricPrimitive New(GraphicsDevice device, float sizeX, float sizeY, int tessellation, Vector2 uvFactor, bool toLeftHanded = false)
+            {
                 if (tessellation < 1)
                 {
                     throw new ArgumentOutOfRangeException("tessellation", "tessellation must be > 0");
@@ -66,7 +84,7 @@ namespace SharpDX.Toolkit.Graphics
                     for (int x = 0; x < (tessellation+1); x++)
                     {
                         var position = new Vector3(-sizeX + deltaX * x, sizeY - deltaY * y, 0);
-                        var texCoord = new Vector2(1.0f * x / tessellation, 1.0f * y / tessellation);
+                        var texCoord = new Vector2(1.0f * x / tessellation * uvFactor.X, 1.0f * y / tessellation * uvFactor.Y);
                         vertices[vertexCount++] = new VertexPositionNormalTexture(position, normal, texCoord);
                     }
                 }
