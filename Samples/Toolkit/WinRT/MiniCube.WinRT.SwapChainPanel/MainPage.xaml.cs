385c093a1d8e4933cd84d81d8c6416ee3384193d
ArtiomCiumac <artiom.ciumac@gmail.com>
2014-02-12 12:05:50 +0200

    [Toolkit.Samples] Added MiniCube sampel for Win 8.1 SwapChainPanel control. Implemented as separate solution to not break building of other samples on older OS.

---
+++ Samples/Toolkit/WinRT/MiniCube.WinRT.SwapChainPanel/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MiniCube.WinRT.SwapChainPanel
+{
+    using SharpDX.Toolkit;
+
+    public sealed partial class MainPage
+    {
+        private readonly Game _game;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _game = new MiniCubeGame();
+            _game.Run(panel);
+        }
+    }
+}
