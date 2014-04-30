778761502b41febc51057a0d0da4f7fb0e3abba5
Alexandre Mutel <alexandre_mutel@live.com>
2014-04-30 22:38:34 +0900

    [Build] Merging SharpDXWP8.sln into SharpDX.sln. We are now back to a single solution for building SharpDX. Hurra!

--- MakeSharpDX.Desktop.Metro.WP81.VS2013.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set PATH=C:\Program Files (x86)\Git\bin\;%PATH%
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\vc\vcvarsall.bat" x86
-msbuild /tv:4.0 /p:BuildSignedSharpDX=false /t:DesktopNet20;DesktopNet40;MetroWin8;DesktopDx112;WP81 /verbosity:quiet /clp:ErrorsOnly /fl /flp:logfile=BuildErrors.log;Append=true %* SharpDX.build
--- MakeSharpDX.WP8.VS2012.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-set PATH=C:\Program Files (x86)\Git\bin\;%PATH%
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\vc\vcvarsall.bat" x86
-echo BUILD WP8-x86, WP8-ARM >> BuildErrors.log
-msbuild /tv:4.0 /p:BuildSignedSharpDX=false /t:WP8-x86;WP8-ARM /verbosity:quiet /clp:ErrorsOnly /fl /flp:logfile=BuildErrors.log;Append=true %* SharpDX.build
