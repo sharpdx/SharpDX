0282319e8787e5b2bba50817b794c0868b8079b2
Alexandre Mutel <alexandre_mutel@live.com>
2014-05-10 09:02:49 +0900

    [Build] Switch to 2.6.0

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -28,8 +28,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly:AssemblyVersion("2.5.1")]
-[assembly:AssemblyFileVersion("2.5.1")]
+[assembly:AssemblyVersion("2.6.0")]
+[assembly:AssemblyFileVersion("2.6.0")]
 
 [assembly: NeutralResourcesLanguage("en-us")]
 
