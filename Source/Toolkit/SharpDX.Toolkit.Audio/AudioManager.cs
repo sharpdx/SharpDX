619fb993952f0f587537d2d7a6ce1ef3c53a3c88
Alexandre Mutel <alexandre_mutel@live.com>
2014-06-16 21:27:17 +0900

    [Build] Update license date

--- Source/Toolkit/SharpDX.Toolkit.Audio/AudioManager.cs
+++ Source/Toolkit/SharpDX.Toolkit.Audio/AudioManager.cs
@@ -1,6 +1,6 @@
 ﻿using SharpDX.Toolkit.Content;
 
-// Copyright (c) 2010-2013 SharpDX - SharpDX Team
+// Copyright (c) 2010-2014 SharpDX - SharpDX Team
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
