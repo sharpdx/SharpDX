12588327c37cf18e20ccea7116147aedbfa86a65
Artiom Ciumac <artiom.ciumac@gmail.com>
2014-06-24 15:40:51 +0100

    [SharpDX] Removed Utilities.LoadLibrary from WinRT and WP8 builds (#420).

--- Source/SharpDX/Utilities.cs
+++ Source/SharpDX/Utilities.cs
@@ -1414,6 +1414,7 @@ namespace SharpDX
 #endif
 #endif
 
+#if !W8CORE
         /// <summary>
         /// Loads a native library.
         /// </summary>
@@ -1428,21 +1429,6 @@ namespace SharpDX
             return result;
         }
 
-#if WP8
-        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-        private delegate IntPtr LoadLibraryDelegate([MarshalAs(UnmanagedType.LPWStr)] string lpFileName, int reserved = 0);
-        private static LoadLibraryDelegate loadLibrary_;
-        private static LoadLibraryDelegate LoadLibrary_
-        {
-            get { return loadLibrary_ ?? (loadLibrary_ = (LoadLibraryDelegate)Marshal.GetDelegateForFunctionPointer(new IntPtr(SharpDX.WP8.Interop.LoadPackagedLibrary()), typeof(LoadLibraryDelegate))); }
-        }
-#elif WIN8METRO && !WP81
-        [DllImport("kernel32", EntryPoint = "LoadPackagedLibrary", SetLastError = true)]
-        static extern IntPtr LoadLibrary_(string lpFileName, int reserved = 0);
-#elif WP81
-        [DllImport("PhoneAppModelHost", EntryPoint = "LoadPackagedLibrary", SetLastError = true)]
-        static extern IntPtr LoadLibrary_(string lpFileName, int reserved = 0);
-#else
         [DllImport("kernel32", EntryPoint = "LoadLibrary", SetLastError = true, CharSet = CharSet.Unicode)]
         static extern IntPtr LoadLibrary_(string lpFileName);
 #endif
