0eacfba0c11f9ddd48617cdab0e3a554a2b7c2ed
AvengerDr <avengerdragon@gmail.com>
2014-07-14 15:22:36 +0100

    Revert "Fixed serialization of Dictionary reference values"
    This reverts commit a4e4673df86f866ebd374ce7efe31feddf6a642e.

--- Source/SharpDX/Serialization/BinarySerializer.cs
+++ Source/SharpDX/Serialization/BinarySerializer.cs
@@ -1213,7 +1213,7 @@ namespace SharpDX.Serialization
                 for (int i = 0; i < count; i++)
                 {
                     TKey localKey = default(TKey);
-                    TValue localValue = Utilities.IsValueType(typeof(TValue)) ? default(TValue) : new TValue();
+                    TValue localValue = default(TValue);
                     keySerializer(ref localKey);
                     localValue.Serialize(this);
                 }
