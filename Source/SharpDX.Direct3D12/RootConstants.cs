d4d52690e442a0af00c0d872184fbd5e3d2e23d0
Alexandre Mutel <alexandre_mutel@live.com>
2015-08-19 21:40:31 +0900

    [Direct3D12] Add RootConstants and RootDescriptor constructors

---
+++ Source/SharpDX.Direct3D12/RootConstants.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2010-2013 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+namespace SharpDX.Direct3D12
+{
+    public partial struct RootConstants
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RootConstants"/> struct.
+        /// </summary>
+        /// <param name="shaderRegister">The shader register.</param>
+        /// <param name="registerSpace">The register space.</param>
+        /// <param name="value32BitCount">The number of constants that occupy a single shader slot (these constants appear like a single constant buffer). All constants occupy a single root signature bind slot. .</param>
+        public RootConstants(int shaderRegister, int registerSpace, int value32BitCount)
+        {
+            ShaderRegister = shaderRegister;
+            RegisterSpace = registerSpace;
+            Value32BitCount = value32BitCount;
+        }
+    }
+}
