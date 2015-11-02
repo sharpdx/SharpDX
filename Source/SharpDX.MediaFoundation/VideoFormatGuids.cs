f596c655c4c6e8078b94959bf9ff2b13926f0261
mrvux <Julien@mrvux.com>
2015-11-02 23:34:46 +0100

    [MediaFoundation] Add extra static method in VideoFormatGuids to retrieve Guid from FourCC

---
+++ Source/SharpDX.MediaFoundation/VideoFormatGuids.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpDX.MediaFoundation
+{
+    public static partial class VideoFormatGuids
+    {
+        /// <summary>
+        /// Returns a standard Media foundation GUID format from a FourCC input
+        /// </summary>
+        /// <param name="fourCC">FourCC input</param>
+        /// <returns>Media foundation unique ID</returns>
+        public static Guid FromFourCC(SharpDX.Multimedia.FourCC fourCC)
+        {
+            return new Guid(string.Concat(fourCC.ToString(), "-0000-0010-8000-00aa00389b71"));
+        }
+    }
+}
