ae90165fe92dd80efd43b0c5a8998c3865d55996
BuildTools <unconfigured@null.spigotmc.org>
2016-02-05 17:53:47 -0500

    Added Direct3D11On12 Support
    Added the Mapping for Direct3D11On12 into a new Project. It was not
    added to the Direct3D12 project to avoid the need for a D3D11 dependency
    in D3D12. Added a Class to Call the Function to Create a D3D11 device
    from a D3D12 device,

---
+++ Source/SharpDX.Direct3D11On12/AssemblyDoc.cs
@@ -0,0 +1,32 @@
+// Copyright (c) 2010-2014 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+namespace SharpDX.Direct3D11
+{
+    /// <summary>
+    /// The <see cref="A:SharpDX.Direct3D11"/> assembly provides managed Direct3D11 API.
+    /// </summary>
+    /// <msdn-id>ff476080</msdn-id>
+    /// <unmanaged>Direct3D11</unmanaged>	
+    /// <unmanaged-short>Direct3D11</unmanaged-short>	
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    class AssemblyDoc
+    {
+    }
+}
