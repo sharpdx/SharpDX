4a066d75d9c421283bf99976883e8bca817728da
AndrewSt <andrew@andrewst.com>
2016-06-02 10:44:51 +0300

    [Direct3D11] Improve support Direct3D 11.3

---
+++ Source/SharpDX.Direct3D11/Texture2D1.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) 2010-2014 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+
+namespace SharpDX.Direct3D11
+{
+    public partial class Texture2D1
+    {
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "T:SharpDX.Direct3D11.Texture2D1" /> class.
+        /// </summary>
+        /// <param name = "device">The device with which to associate the texture.</param>
+        /// <param name = "description">The description of the texture.</param>
+        /// <msdn-id>ff476521</msdn-id>	
+        /// <unmanaged>HRESULT ID3D11Device3::CreateTexture2D1([In] const D3D11_TEXTURE2D_DESC1* pDesc,[In, Buffer, Optional] const D3D11_SUBRESOURCE_DATA* pInitialData,[Out, Fast] ID3D11Texture2D1** ppTexture2D)</unmanaged>	
+        /// <unmanaged-short>ID3D11Device3::CreateTexture2D1</unmanaged-short>	
+        public Texture2D1(Device3 device, Texture2DDescription1 description)
+            : base(IntPtr.Zero)
+        {
+            device.CreateTexture2D1(ref description, null, this);
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "T:SharpDX.Direct3D11.Texture2D1" /> class.
+        /// </summary>
+        /// <param name = "device">The device with which to associate the texture.</param>
+        /// <param name = "description">The description of the texture.</param>
+        /// <param name = "data">An array of initial texture data for each subresource.</param>
+        /// <msdn-id>ff476521</msdn-id>	
+        /// <unmanaged>HRESULT ID3D11Device3::CreateTexture2D1([In] const D3D11_TEXTURE2D_DESC1* pDesc,[In, Buffer, Optional] const D3D11_SUBRESOURCE_DATA* pInitialData,[Out, Fast] ID3D11Texture2D1** ppTexture2D)</unmanaged>	
+        /// <unmanaged-short>ID3D11Device3::CreateTexture2D1</unmanaged-short>	
+        public Texture2D1(Device3 device, Texture2DDescription1 description, params DataRectangle[] data)
+            : base(IntPtr.Zero)
+        {
+            DataBox[] subResourceDatas = null;
+
+            if (data != null && data.Length > 0)
+            {
+                subResourceDatas = new DataBox[data.Length];
+                for (int i = 0; i < subResourceDatas.Length; i++)
+                {
+                    subResourceDatas[i].DataPointer = data[i].DataPointer;
+                    subResourceDatas[i].RowPitch = data[i].Pitch;
+                }
+            }
+
+            device.CreateTexture2D1(ref description, subResourceDatas, this);
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "T:SharpDX.Direct3D11.Texture2D1" /> class.
+        /// </summary>
+        /// <param name = "device">The device with which to associate the texture.</param>
+        /// <param name = "description">The description of the texture.</param>
+        /// <param name = "data">An array of initial texture data for each subresource.</param>
+        /// <msdn-id>ff476521</msdn-id>	
+        /// <unmanaged>HRESULT ID3D11Device3::CreateTexture2D1([In] const D3D11_TEXTURE2D_DESC1* pDesc,[In, Buffer, Optional] const D3D11_SUBRESOURCE_DATA* pInitialData,[Out, Fast] ID3D11Texture2D** ppTexture2D)</unmanaged>	
+        /// <unmanaged-short>ID3D11Device3::CreateTexture2D1</unmanaged-short>	
+        public Texture2D1(Device3 device, Texture2DDescription1 description, DataBox[] data)
+            : base(IntPtr.Zero)
+        {
+            device.CreateTexture2D1(ref description, data, this);
+        }
+
+        /// <inheritdoc/>
+        public override int CalculateSubResourceIndex(int mipSlice, int arraySlice, out int mipSize)
+        {
+            var desc = Description;
+            mipSize = CalculateMipSize(mipSlice, desc.Height);
+            return CalculateSubResourceIndex(mipSlice, arraySlice, desc.MipLevels);
+        }
+  }
+}
