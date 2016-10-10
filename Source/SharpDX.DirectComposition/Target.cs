6221710c7e6f91caa72b67bb45481948b8d53082
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-10-11 04:46:54 +0530

    Add missing method to create Target from `Device`
    This is method is the only way to create a Target in Windows 8, which doesn't have `DesktopDevice`. And this method is unfortuately missing.

--- Source/SharpDX.DirectComposition/Target.cs
+++ Source/SharpDX.DirectComposition/Target.cs
@@ -18,5 +18,17 @@ namespace SharpDX.DirectComposition
 		{
 			return desktopDevice.CreateTargetForHwnd(hwnd, topmost);
 		}
+        
+        /// <summary>
+		/// Creates a composition target object that is bound to the window that is represented by the specified window handle.
+		/// </summary>
+		/// <param name="device">A device the target is to be associated with.</param>
+		/// <param name="hwnd">The window to which the composition object will be bound. This cannot be null.</param>
+		/// <param name="topmost">TRUE if the visual tree should be displayed on top of the children of the window specified by the hwnd parameter; otherwise, the visual tree is displayed behind the children.</param>
+		/// <returns></returns>
+		public static Target FromHwnd(Device device, IntPtr hwnd, bool topmost)
+		{
+			return device.CreateTargetForHwnd(hwnd, topmost);
+		}
 	}
 }
