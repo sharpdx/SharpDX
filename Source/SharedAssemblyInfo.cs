5ed54fafffe5e1ba78efc6fb5efa9c41854e2ce0
Alexandre Mutel <alexandre_mutel@live.com>
2016-10-19 11:33:56 +0200

    Bump version to 3.1.0

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -28,8 +28,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly:AssemblyVersion("3.0.2")]
-[assembly:AssemblyFileVersion("3.0.2")]
+[assembly:AssemblyVersion("3.1.0")]
+[assembly:AssemblyFileVersion("3.1.0")]
 
 [assembly: NeutralResourcesLanguage("en-us")]
 
