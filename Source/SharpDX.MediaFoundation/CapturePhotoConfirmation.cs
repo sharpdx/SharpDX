d093456b211e6f97516b08ec7810518e25eb7a81
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-01-03 16:28:08 -0600

    Improved CapturePhotoConfirmation mapping.

---
+++ Source/SharpDX.MediaFoundation/CapturePhotoConfirmation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpDX.MediaFoundation
+{
+    public partial class CapturePhotoConfirmation
+    {
+        public IAsyncCallback PhotoConfirmationCallback
+        {
+            set
+            {
+                SetPhotoConfirmationCallback_(AsyncCallbackShadow.ToIntPtr(value));
+            }
+        }
+    }
+}
