cb9dad286f43e6af34d9df3f1455dbadf57b3cda
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-01-03 18:03:15 -0600

    Add desktop check and documentation to CapturePhotoConfirmation.

--- Source/SharpDX.MediaFoundation/CapturePhotoConfirmation.cs
+++ Source/SharpDX.MediaFoundation/CapturePhotoConfirmation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if DESKTOP_APP
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,14 @@ namespace SharpDX.MediaFoundation
 {
     public partial class CapturePhotoConfirmation
     {
+        /// <summary>	
+        /// No documentation.	
+        /// </summary>	
+        /// <param name="notificationCallbackRef">No documentation.</param>	
+        /// <returns>No documentation.</returns>	
+        /// <include file='.\..\Documentation\CodeComments.xml' path="/comments/comment[@id='IMFCapturePhotoConfirmation::SetPhotoConfirmationCallback']/*"/>	
+        /// <unmanaged>HRESULT IMFCapturePhotoConfirmation::SetPhotoConfirmationCallback([In] IMFAsyncCallback* pNotificationCallback)</unmanaged>	
+        /// <unmanaged-short>IMFCapturePhotoConfirmation::SetPhotoConfirmationCallback</unmanaged-short>	
         public IAsyncCallback PhotoConfirmationCallback
         {
             set
@@ -17,3 +26,5 @@ namespace SharpDX.MediaFoundation
         }
     }
 }
+
+#endif
