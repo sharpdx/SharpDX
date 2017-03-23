c10a662376084155a00983125ced82845fa5e2bc
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-03-22 22:09:35 -0500

    Updated DirectManipulation to the new project system

--- Source/SharpDX.DirectManipulation/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SharpDX.DirectManipulation")]
-[assembly: AssemblyProduct("SharpDX.DirectManipulation")]
-[assembly: AssemblyDescription("Assembly providing DirectX - DirectManipulation managed API")]
