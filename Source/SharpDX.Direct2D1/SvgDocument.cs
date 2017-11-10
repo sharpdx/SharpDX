c578030a55792ee902735afe73b76331064b8a7a
Julien Vulliet <julien@mrvux.com>
2017-11-11 00:02:30 +0300

    Remove some methods to try get element, as if an element is not found, Direct2D only returns a zero pointer with a S_Ok hresult

--- Source/SharpDX.Direct2D1/SvgDocument.cs
+++ Source/SharpDX.Direct2D1/SvgDocument.cs
@@ -11,7 +11,7 @@ namespace SharpDX.Direct2D1
         /// Finds an svg element by id
         /// </summary>
         /// <param name="id">Id to lookup for</param>
-        /// <returns>SvgElement</returns>
+        /// <returns>SvgElement if found, null otherwise</returns>
         public SvgElement FindElementById(string id)
         {
             SharpDX.Result __result__;
@@ -20,20 +20,6 @@ namespace SharpDX.Direct2D1
 
             __result__.CheckError();
             return svgElement;
-        }
-
-        /// <summary>
-        /// Try to find an element by id
-        /// </summary>
-        /// <param name="id">id to search for</param>
-        /// <param name="svgElement">When this method completes, contains the relevant element (if applicable)</param>
-        /// <returns>true if element has been found, false otherwise</returns>
-        public bool TryFindElementById(string id, out SvgElement svgElement)
-        {
-            SharpDX.Result __result__;
-            __result__ = TryFindElementById_(id, out svgElement);
-
-            return __result__.Code >= 0;
         }
     }
 }
