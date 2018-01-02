4ec0a1c2c06f0b61f4fc2cd2b94150ae9367e1b4
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-01-01 20:40:06 -0600

    Clean up now unneeded files.

--- Build/build.cmd
+++
@@ -1,4 +0,0 @@
-msbuild SharpDX-Tools.sln /t:Clean;Build /m /p:Configuration=Release;Platform="Any CPU"
-msbuild SharpDX-Sources.sln /t:Clean /m /p:Configuration=Release;Platform="Any CPU"
-dotnet restore SharpDX-Sources.sln
-msbuild SharpDX-Sources.sln /t:Pack /p:Configuration=Release;Platform="Any CPU";NuGetBuildTasksPackTargets="workaround" /m
---
+++ build.cmd
@@ -0,0 +1,2 @@
+msbuild SharpDX.sln /t:Clean /m /p:Configuration=Release;Platform="Any CPU"
+msbuild SharpDX.sln /restore /t:Build;Pack /p:Configuration=Release;Platform="Any CPU" /m
