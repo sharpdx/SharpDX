975a68931d82c54e62329138fd0509cfaf319687
Andrew Heap <aheap@users.on.net>
2019-03-30 19:19:56 +1100

    Addressed Issue #1084 - GetPickRay using integer coordinates

--- Source/SharpDX.Mathematics/Ray.cs
+++ Source/SharpDX.Mathematics/Ray.cs
@@ -284,7 +284,7 @@ namespace SharpDX
         /// <param name="viewport"><see cref="ViewportF"/>.</param>
         /// <param name="worldViewProjection">Transformation <see cref="Matrix"/>.</param>
         /// <returns>Resulting <see cref="Ray"/>.</returns>
-        public static Ray GetPickRay(int x, int y, ViewportF viewport, Matrix worldViewProjection)
+        public static Ray GetPickRay(float x, float y, ViewportF viewport, Matrix worldViewProjection)
         {
             var nearPoint = new Vector3(x, y, 0);
             var farPoint = new Vector3(x, y, 1);
@@ -298,6 +298,20 @@ namespace SharpDX
             direction.Normalize();
 
             return new Ray(nearPoint, direction);
+        }
+
+        /// <summary>
+        /// Calculates a world space <see cref="Ray"/> from 2d screen coordinates.
+        /// Integer version converts values to float
+        /// </summary>
+        /// <param name="x">X coordinate on 2d screen.</param>
+        /// <param name="y">Y coordinate on 2d screen.</param>
+        /// <param name="viewport"><see cref="ViewportF"/>.</param>
+        /// <param name="worldViewProjection">Transformation <see cref="Matrix"/>.</param>
+        /// <returns>Resulting <see cref="Ray"/>.</returns>
+        public static Ray GetPickRay(int x, int y, ViewportF viewport, Matrix worldViewProjection)
+        {
+            return GetPickRay((float)x, (float)y, viewport, worldViewProjection);
         }
 
         /// <summary>
